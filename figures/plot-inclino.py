7d73aeda6485b86a68c76585d4450dbc13ad7590
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-08-26 11:20:27 +0200

    Add inclinometer (pressure) plot.

--- figures/plot-inclino.py
+++ figures/plot-inclino.py
@@ -13,17 +13,20 @@ fig, grid = plt.subplots(2, 1, sharex=True)
 # parameters
 for bh in [1, 2]:
     ax = grid[bh-1]
-    filename = 'data/processed/bowdoin-temperature-bh%d.txt' % bh
+    filename = 'data/processed/bowdoin-inclino-bh%d.txt' % bh
 
     # read in a record array
     a = np.genfromtxt(filename, delimiter=',',
-                      names=True, #('date', 'pressure'),
-                      converters={'date': mkdate},
+                      missing_values=9999.0, usemask=True,
+                      names=True, converters={'date': mkdate},
                       dtype=None)
 
     # plot
-    for res in range(1, 17):
-        ax.plot_date(a['date'], a['res%02d' % res], '-')
+    for site in a.dtype.names[1:]:
+        ax.plot_date(a['date'], a[site], '-')
+
+    # set axes limits
+    ax.set_ylim(0.0, 40.0)
 
 # save
-fig.savefig('plot-temperature')
+fig.savefig('plot-inclino')
