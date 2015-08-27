029455e3badaf2cec17214f8c230669fa5ce4e58
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-08-27 12:05:04 +0200

    Add conversion from resistances to temperatures.

--- figures/data/preprocess-temperature.py
+++ figures/data/preprocess-temperature.py
@@ -7,6 +7,15 @@ import numpy as np
 # for each borehole
 for bh in [1, 2]:
 
+    # read rearranged calibration coefficients
+    # borehole 1: BH2A[1-9] + BH2B[1-7]
+    # borehole 2: BH1A[1-9] + BH1B[1-4,7,5-6]
+    coefpath = 'original/temperature/Th-Bowdoin-%i_Coefs.dat' % bh
+    coefs = np.loadtxt(coefpath)
+    a1 = coefs[:,0]
+    a2 = coefs[:,1]
+    a3 = coefs[:,2]
+
     # input and output file names
     ifilename = 'original/temperature/Th-Bowdoin-%i_Therm.dat' % bh
     ofilename = 'processed/bowdoin-temperature-bh%d.txt' % bh
@@ -27,6 +36,8 @@ for bh in [1, 2]:
             # preprocess each row
             for row in reader:
                 datestring = row[0]
-                date = datetime.datetime.strptime(datestring, '%Y-%m-%d %H:%M:%S')
-                values = map(float, row[3:19])  # use resistances only
-                writer.writerow([date] + values)
+                date = datetime.datetime.strptime(datestring,
+                                                  '%Y-%m-%d %H:%M:%S')
+                res = np.array(map(float, row[3:19]))
+                temp = 1 / (a1 + a2*np.log(res) + a3*(np.log(res))**3) - 273.15
+                writer.writerow([date] + list(temp))
