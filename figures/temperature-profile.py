ae506e421faa6388f35ccc0c837597183d8c44ee
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-10-30 14:02:39 +0100

    Fix temperature profile.

--- figures/temperature-profile.py
+++ figures/temperature-profile.py
@@ -1,47 +1,41 @@
 #!/usr/bin/env python2
 
-import datetime
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # dates to plot
-targets = [datetime.datetime(2014, m, 25, 6, 0) for m in range(7, 13)]
-
-# date converter
-mkdate = lambda s: datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+targets = ['2014-%02d-25 06:00' % m for m in range(7, 13)]
 
 # initialize figure
 fig, grid = plt.subplots(1, 2, sharey=True)
 
-# parameters
-for bh in [1, 2]:
-    ax = grid[bh-1]
-    filename = 'data/processed/bowdoin-temperature-bh%d.txt' % bh
+# for each borehole
+for i, bh in enumerate(['upstream', 'downstream']):
+    ax = grid[i]
+    filename = 'data/processed/bowdoin-temperature-%s.csv' % bh
 
     # calculate sensor depths
-    if bh == 1:
+    if bh == 'downstream':
         depth = np.hstack([-243.7 + 20.0*np.arange(9),
                            -8.75 + 20.0*np.arange(-4,3)])
-    if bh == 2:
+    if bh == 'upstream':
         depth = np.hstack([-265.3 + 20.0*np.arange(9),
                            -6.6 + 20.0*np.array([-5, -4, -3, -2, +1, -1, 0])])
 
-    # check which 
+    # read data
+    df = pd.read_csv(filename, parse_dates=True, index_col='date')
+
+    # order columns by depth
     order = np.argsort(depth)
-
-
-    # read in a record array
-    a = np.genfromtxt(filename, delimiter=',',
-                      names=True, #('date', 'pressure'),
-                      converters={'date': mkdate},
-                      dtype=None)
 
     # plot
     for tg in targets:
-        row = np.searchsorted(a['date'], tg)
-        date = a[row][0]
-        temp = np.array([a[row][col] for col in range(1, 17)])
+        row = df.index.searchsorted(tg)
+        date = df.index[row]
+        temp = df.iloc[row]
         ax.plot(temp[order], depth[order], '-', label=date)
+        ax.set_title(bh)
 
     # add horizontal lines
     ax.axhline(depth[0], c='k')
