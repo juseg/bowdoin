ef83c8d5bbc47e7ecf6e71599584f03567b03a35
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-11-15 15:59:00 +0100

    Plot min, max and avg temperature profiles.

--- figures/temperature-profile.py
+++ figures/temperature-profile.py
@@ -4,14 +4,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+boreholes = ['downstream', 'upstream']
+colors = ['b', 'r']
+
+def get_profiles(depth, temp):
+    """Return avg, min and max temperature profile from data frame."""
+
+    # remove sensors above the ground
+    inicecols = depth < 0.0
+    temp = temp[temp.columns[inicecols]]
+    depth = depth[inicecols]
+
+    # order by depth
+    ordercols = np.argsort(depth)
+    depth = depth[ordercols]
+    temp = temp[ordercols]
+
+    # extract values
+    tmin = temp.min().values
+    tavg = temp.mean().values
+    tmax = temp.max().values
+    z = depth.values
+
+    return z, tmin, tavg, tmax
+
 # dates to plot
-date = '2015-02-01 00:00:00'
+start = '2014-10-15'
+end = '2015-07-15'
 
 # initialize figure
 fig, grid = plt.subplots(1, 2, sharey=True)
 
 # for each borehole
-for i, bh in enumerate(['downstream', 'upstream']):
+for i, bh in enumerate(boreholes):
     ax = grid[i]
 
     # read temperature values
@@ -26,26 +51,31 @@ for i, bh in enumerate(['downstream', 'upstream']):
     filename = 'data/processed/bowdoin-inclino-depth-%s.csv' % bh
     tilt_depth = pd.read_csv(filename, header=None, index_col=0, squeeze=True)
 
-    # select date
-    temp_temp = temp_temp.loc[date].values
-    tilt_temp = tilt_temp.loc[date].values
-
     # FIXME
     tilt_depth *= -1
     tilt_temp *= 1e-3
 
-    # order by depth and mask above surface
-    order = np.argsort(temp_depth)
-    temp_depth = temp_depth[order]
-    temp_temp = np.ma.masked_where(temp_depth > 0.0, temp_temp[order])
+    # resample and concatenate
+    tilt_temp = tilt_temp.resample('1D')[start:end]
+    temp_temp = temp_temp.resample('1D')[start:end]
+    join_temp = pd.concat((temp_temp, tilt_temp), axis=1)
+    join_depth = pd.concat((temp_depth, tilt_depth))
+
+    # extract profiles
+    temp_z, temp_tmin, temp_tavg, temp_tmax = get_profiles(temp_depth, temp_temp)
+    tilt_z, tilt_tmin, tilt_tavg, tilt_tmax = get_profiles(tilt_depth, tilt_temp)
+    join_z, join_tmin, join_tavg, join_tmax = get_profiles(join_depth, join_temp)
 
     # plot
-    ax.plot(temp_temp, temp_depth, 'o-')
-    ax.plot(tilt_temp, tilt_depth, 'o-')
+    ax.fill_betweenx(join_z, join_tmin, join_tmax,
+                     facecolor=colors[i], edgecolor='none', alpha=0.2)
+    ax.plot(join_tavg, join_z, '-', c=colors[i])
+    ax.plot(temp_tavg, temp_z, 'o', c=colors[i])
+    ax.plot(tilt_tavg, tilt_z, '^', c=colors[i])
     ax.set_title(bh)
 
     # add horizontal lines
-    ax.axhline(temp_depth[0], c='k')
+    ax.axhline(join_z[0], c='k')
     ax.axhline(0.0, c='k')
 
 # save
