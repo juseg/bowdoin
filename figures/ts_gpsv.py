636b48c40acd05b118b149a38c1b8fc8c412eb4b
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-11-16 17:46:59 +0100

    Put nice colors.

--- figures/ts_gpsv.py
+++ figures/ts_gpsv.py
@@ -9,7 +9,7 @@ fig, ax = plt.subplots(1, 1)
 
 # plot GPS velocity
 ts = ut.io.load_data('dgps', 'velocity', 'upstream')['vh'].resample('15T')
-ut.pl.rolling_plot(ax, ts, 4*6, c='g')
+ut.pl.rolling_plot(ax, ts, 4*6, c=ut.colors[2])
 ts.plot(ax=ax, color='g', ls='', marker='.', markersize=0.5)
 
 # add label
--- figures/util/__init__.py
+++ figures/util/__init__.py
@@ -6,5 +6,11 @@
 import io
 import pl
 
+# the color brewer Paired palette
+# light/drak blue, green, red, orange, purple, brown
+palette = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c',
+           '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928']
+
+# borehole properties
 boreholes = ['downstream', 'upstream']
-colors = ['r', 'b']
+colors = palette[1], palette[5], palette[3]  # the third color for GPS
