82cb1ae8192f10cdea402bc35364ff00115684cd
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-02-29 17:27:11 +0100

    Simplify animation script.

--- figures/anim_dvel.py
+++ figures/anim_dvel.py
@@ -1,69 +1,32 @@
 #!/usr/bin/env python2
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import util as ut
-import scipy.optimize
-
-
-def powerfit(x, y, deg, **kwargs):
-    logx = np.log(x)
-    logy = np.log(y)
-    p = np.polyfit(logx, logy, deg, **kwargs)
-    return p
+import pandas as pd
+from matplotlib.animation import FuncAnimation
 
 # dates to plot
-start = '2014-07-01'
-end = '2015-08-01'
+start = '2014-11-01'
+end = '2015-12-01'
 
-# for each borehole
-exz = [None, None]
-depth = [None, None]
-depth_base = [None, None]
-fitparams = [None, None]
-for i, bh in enumerate(ut.boreholes):
+# load data
+exz = {bh: ut.io.load_strain_rate(bh, '1D')[start:end] for bh in ut.boreholes}
+depth = {bh: ut.io.load_depth('tiltunit', bh).squeeze() for bh in ut.boreholes}
+depth_base = {bh: ut.io.load_depth('pressure', bh).squeeze() for bh in ut.boreholes}
 
-    # read tilt unit tilt
-    tiltx = ut.io.load_data('tiltunit', 'tiltx', bh).resample('1D')[start:end]
-    tilty = ut.io.load_data('tiltunit', 'tilty', bh).resample('1D')[start:end]
-    depth[i] = ut.io.load_depth('tiltunit', bh).squeeze()
-    depth_base[i] = ut.io.load_depth('pressure', bh).squeeze()
+# ignore two lowest units on upstream borehole
+broken = ['unit02', 'unit03']
+depth['upstream'].drop(broken, inplace=True)
+exz['upstream'].drop(broken, axis='columns', inplace=True)
 
-    # compute horizontal shear rate
-    dt = 1.0/365
-    exz_x = np.sin(tiltx).diff()
-    exz_y = np.sin(tilty).diff()
-    exz[i] = np.sqrt(exz_x**2+exz_y**2)/dt
+# concatenate
+exz = pd.concat(exz, axis='columns', keys=ut.boreholes)
+depth = pd.concat(depth)
+depth_base = pd.Series(depth_base)
 
-    # ignore two lowest units on upstream borehole
-    if bh == 'upstream':
-        broken = ['unit02', 'unit03']
-        depth[i].drop(broken, inplace=True)
-        exz[i].drop(broken, axis='columns', inplace=True)
+# drawing function for animation
+def draw(date, fig, grid, xlabel):
 
-    # fit to a power law with exp(C) = A * (rhoi*g*sin(alpha))**n
-    g = 9.80665     # gravity
-    rhoi = 910.0    # ice density
-    sina = 0.03     # FIXME: approx. value from MEASURES
-    n, C = powerfit(depth[i], exz[i].T, 1)
-    fitparams[i] = pd.DataFrame(index=exz[i].index, data={'n': n, 'C': C})
-
-# contatenate
-fitparams = pd.concat(fitparams, axis=1, keys=ut.boreholes)
-
-# initialize figure
-fig, grid = ut.pl.subplots_mm(nrows=1, ncols=2, sharex=True, sharey=True,
-                             left=10.0, bottom=10.0, right=5.0, top=5.0,
-                             wspace=5.0, hspace=5.0)
-
-# add common labels
-figw, figh = fig.get_size_inches()*25.4
-xlabel = fig.text(0.5, 2.5/figh, '', ha='center')
-ylabel = fig.text(2.5/figw, 0.5, 'depth (m)', va='center', rotation='vertical')
-
-# loop over dates
-for i, date in enumerate(fitparams.index):
+    # get date string
     datestring = date.strftime('%Y-%m-%d')
     print 'plotting frame at %s ...' % datestring
 
@@ -75,47 +38,30 @@ for i, date in enumerate(fitparams.index):
         ax = grid[j]
         ax.cla()
         c = ut.colors[j]
-        n = fitparams[bh, 'n'][date]
-        C = fitparams[bh, 'C'][date]
-        A = np.exp(C) / (rhoi*g*sina)**n
 
-        # plot deformation profile
-        depth_fit = np.linspace(0.0, depth_base[j], 11)
-        e_fit = np.exp(C) * depth_fit**n
-        v_fit = 2*np.exp(C)/(n+1) * (depth_base[j]**(n+1) - depth_fit**(n+1))
-        v_obs = 2*np.exp(C)/(n+1) * (depth_base[j]**(n+1) - depth[j]**(n+1))
+        # plot velocity profile
+        ut.pl.plot_vsia_profile(depth[bh], exz[bh].loc[datestring],
+                                depth_base[bh], ax=ax, c=c)
 
-        # plot velocity profiles
-        ax.plot(v_fit, depth_fit, c=c)
-        ax.fill_betweenx(depth_fit, 0.0, v_fit, color=c, alpha=0.25)
-        ax.set_title(bh)
-
-        # add velocity arrows
-        for d, v in zip(depth[j], v_obs):
-            ax.arrow(0.0, d, v, 0.0, fc='none', ec=c,
-                     head_width=5.0, head_length=1.0, length_includes_head=True)
-
-        # add tilt arrows
-        try:
-            ax.quiver(v_obs, depth[j], -exz[j].loc[datestring]*2,
-                      np.sqrt(1-(2*exz[j].loc[datestring])**2),
-                      angles='xy', scale=5.0)
-        except KeyError:
-            pass
-
-        # add horizontal lines
-        ax.axhline(0.0, c='k')
-        ax.axhline(depth_base[j], c='k')
+        # set axes limits
         ax.set_ylim(300.0, 0.0)
         ax.set_xlim(50.0, 0.0)
-
-        # add fit values
-        ax.text(0.05, 0.05, r'A=%.2e$\,Pa^{-n}\,s^{-2}$, n=%.2f' % (A, n),
-                transform=ax.transAxes)
 
     # remove right axis tick labels
     for label in ax.get_yticklabels():
         label.set_visible(False)
 
-    # save
-    fig.savefig('frames/%04d.png' % i)
+# initialize figure and labels
+fig, grid = ut.pl.subplots_mm(nrows=1, ncols=2, sharex=True, sharey=True,
+                             left=10.0, bottom=10.0, right=5.0, top=5.0,
+                             wspace=5.0, hspace=5.0)
+figw, figh = fig.get_size_inches()*25.4
+xlabel = fig.text(0.5, 2.5/figh, '', ha='center')
+ylabel = fig.text(2.5/figw, 0.5, 'depth (m)', va='center', rotation='vertical')
+
+# create animation
+anim = FuncAnimation(fig, draw, frames=exz.index,
+                     fargs=(fig, grid, xlabel))
+
+# save
+anim.save('anim_dvel.mp4', fps=25, dpi=254, bitrate=1024)
