bbdd20ccac5681f2906f4a1acbb5430c5f1b6075
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-02-29 20:17:17 +0100

    Add a timeline and cursor on tilt animation.

--- figures/anim_tilt.py
+++ figures/anim_tilt.py
@@ -6,7 +6,7 @@ from matplotlib.animation import FuncAnimation
 
 # dates to plot
 start = '2014-11-01'
-end = '2015-12-01'
+end = '2015-12-31'
 
 # load data
 exz = {bh: ut.io.load_strain_rate(bh, '1D')[start:end] for bh in ut.boreholes}
@@ -24,14 +24,11 @@ depth = pd.concat(depth)
 depth_base = pd.Series(depth_base)
 
 # drawing function for animation
-def draw(date, fig, grid, xlabel):
+def draw(date, fig, grid, cursor):
 
     # get date string
     datestring = date.strftime('%Y-%m-%d')
     print 'plotting frame at %s ...' % datestring
-
-    # update xlabel
-    xlabel.set_text('ice deformation on %s ($m\,a^{-1}$)' % datestring)
 
     # loop over boreholes
     for j, bh in enumerate(ut.boreholes):
@@ -47,21 +44,49 @@ def draw(date, fig, grid, xlabel):
         ax.set_ylim(300.0, 0.0)
         ax.set_xlim(50.0, 0.0)
 
+    # set y label
+    ax.set_ylabel('depth (m)')
+
     # remove right axis tick labels
     for label in ax.get_yticklabels():
         label.set_visible(False)
 
+    # update cursor
+    cursor.set_data(date, (0.0, 1.0))
+
 # initialize figure and labels
-fig, grid = ut.pl.subplots_mm(nrows=1, ncols=2, sharex=True, sharey=True,
-                             left=10.0, bottom=10.0, right=5.0, top=5.0,
-                             wspace=5.0, hspace=5.0)
-figw, figh = fig.get_size_inches()*25.4
-xlabel = fig.text(0.5, 2.5/figh, '', ha='center')
-ylabel = fig.text(2.5/figw, 0.5, 'depth (m)', va='center', rotation='vertical')
+figw, figh = 120.0, 140.0
+fig, grid = ut.pl.subplots_mm(nrows=1, ncols=2, figsize=(figw, figh),
+                              left=10.0, bottom=50.0, right=5.0, top=5.0,
+                              wspace=5.0, hspace=5.0, sharex=True, sharey=True)
+tsax = fig.add_axes([10.0/figw, 10.0/figh, 1-15.0/figw, 35.0/figh])
+
+# plot time series
+for i, bh in enumerate(ut.boreholes):
+    c = ut.colors[i]
+
+    # fit to a Glen's law
+    n, A = ut.al.glenfit(depth[bh], exz[bh].T)
+
+    # calc deformation velocity
+    vdef = ut.al.vsia(0.0, depth_base[bh], n, A)
+    vdef = pd.Series(index=exz.index, data=vdef)
+
+    # plot
+    vdef.plot(ax=tsax, c=c, label=bh)
+
+# add cursor
+cursor = tsax.axvline(0.0, c='k', lw=0.25)
+
+# set labels
+tsax.set_ylabel(r'deformation velocity ($m\,a^{-1}$)')
+tsax.set_ylim(20.0, 60.0)
+tsax.legend()
 
 # create animation
 anim = FuncAnimation(fig, draw, frames=exz.index,
-                     fargs=(fig, grid, xlabel))
+                     fargs=(fig, grid, cursor))
 
 # save
-anim.save('anim_tilt.mp4', fps=25, dpi=254, bitrate=1024)
+anim.save('anim_tilt.mp4', fps=25, codec='h264')
+anim.save('anim_tilt.ogg', fps=25, codec='theora')
