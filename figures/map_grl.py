5f52dc1a0467e0b1bc92b8cddc9ac3f1ead24018
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-03-08 19:05:56 +0100

    Enlarge and add seismometers on Greenland map.

--- figures/map_grl.py
+++ figures/map_grl.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
                               true_scale_latitude=70.0)
 
     # subregions w, e, s, n
-    grld = (-725e3, +925e3, -0575e3, -3425e3)  # 1650x2850 (55*30x95*30)
+    grld = (-725e3, +900e3, -0550e3, -3425e3)  # 1625x2875 (65*25x115*25)
     qaaq = (-650e3, -450e3, -1325e3, -1125e3)  # 200x200
     bowd = (-547e3, -517e3, -1237e3, -1207e3)  # 30x30
     jako = (-300e3, +000e3, -2350e3, -2050e3)  # 300x300
@@ -71,15 +71,15 @@ if __name__ == '__main__':
     data, extent = open_measures_composite()
 
     # initialize figure
-    figw, figh = 120.0, 100.0
+    figw, figh = 135.0, 120.0
     fig = plt.figure(0, (figw/25.4, figh/25.4))
-    rect1 = [2.5/figw, 2.5/figh, 55.0/figw, 95.0/figh]
-    rect2 = [60.0/figw, 62.5/figh, 35.0/figw, 35.0/figh]
-    rect3 = [60.0/figw, 2.5/figh, 57.5/figw, 57.5/figh]
+    rect1 = [2.5/figw, 2.5/figh, 65.0/figw, 115.0/figh]
+    rect2 = [70.0/figw, 67.5/figh, 50.0/figw, 50.0/figh]
+    rect3 = [70.0/figw, 2.5/figh, 62.5/figw, 62.5/figh]
     ax1 = fig.add_axes(rect1, projection=proj)
     ax2 = fig.add_axes(rect2, projection=proj)
     ax3 = fig.add_axes(rect3, projection=proj)
-    cax = plt.axes([97.5/figw, 62.5/figh, 5.0/figw, 35.0/figh])
+    cax = plt.axes([122.5/figw, 77.5/figh, 2.5/figw, 40.0/figh])
 
     # set rasterization levels
     ax1.set_rasterization_zorder(2.5)
@@ -106,11 +106,11 @@ if __name__ == '__main__':
 
     # add colorbar
     cb = fig.colorbar(im, cax=cax)
-    cb.set_label(r'surface velocity ($m\,yr^{-1}$)')
+    cb.set_label(r'surface velocity ($m\,yr^{-1}$)', labelpad=0.0)
 
     # plot ax3 satellite image
     background = cimgt.MapQuestOpenAerial()
-    ax3.add_image(background, 10)
+    ax3.add_image(background, 11)
 
     # plot locations of camera and boreholes
     llz = {'qaanaaq':   (-69.230556, 77.466667,   0.000000),
@@ -122,18 +122,24 @@ if __name__ == '__main__':
            'bh1':       (-68.555749, 77.691244,  88.694913),  # upstream
            'bh2':       (-68.555685, 77.691307,  87.746263),  # upstream
            'bh3':       (-68.558857, 77.689995,  83.446628),  # downstream
+           'b1501':     (-68.607773, 77.679805,  59.2910),  # 150719 17:30
+           'b1502':     (-68.564605, 77.693997,  96.2647),  # 150719 14:30
+           'b1503':     (-68.554406, 77.706070, 133.6503),  # 150719 13:00
            'camp':      (-68.509920, 77.685890,  70.000000)}
     ax2.plot(*llz['qaanaaq'][:2], c='k', marker='o',  transform=ll)
     ax3.plot(*llz['bh3'][:2], c=ut.colors[0], marker='o', transform=ll)
+    ax3.plot(*llz['b1501'][:2], c=ut.colors[1], marker='s', transform=ll)
     ax3.plot(*llz['cam_lower'][:2], c=ut.palette[7], marker='^', transform=ll)
     ax3.plot(*llz['cam_upper'][:2], c=ut.palette[7], marker='^', transform=ll)
 
     # annotate
-    ax2.text(-600e3, -1250e3, 'Qaanaaq', color='k',
+    ax2.text(-590000, -1250000, 'Qaanaaq', color='k',
              ha='center', fontweight='bold')
     ax3.text(-532500, -1225500, 'boreholes', color=ut.colors[0],
              ha='center', fontweight='bold')
-    ax3.text(-533500, -1229500, 'cams', color=ut.palette[7],
+    ax3.text(-540000, -1229000, 'seismometers', color=ut.colors[1],
+             ha='center', fontweight='bold')
+    ax3.text(-532000, -1229500, 'cameras', color=ut.palette[7],
              ha='center', fontweight='bold')
 
     # add scale
