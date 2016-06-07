717593d5517afdc96b00b24fd9b60227720c829b
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-06-07 09:10:16 +0200

    Plot Takahiro Landsat vel alongside Sentinel.
    Also rename ts_sentinel to ts_satvel.

--- figures/ts_satvel.py
+++ figures/ts_satvel.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import util as ut
+import scipy.io
 
 # initialize figure
 fig, ax = plt.subplots(1, 1)
@@ -28,6 +29,19 @@ vel = df['velocity']
 err = df['error']
 ax.errorbar(mid, vel, xerr=dt/2, yerr=err, c=c, ls='', zorder=3)
 
+# plot landsat velocity
+c = ut.palette[11]
+mat = scipy.io.loadmat('data/satellite/bowdoin-landsat.mat')
+start = ['2014-07-13', '2014-09-06', '2014-10-01', '2015-04-02', '2015-06-14']
+start = pd.to_datetime(start).values  #, unit='D').values
+end = ['2014-09-06', '2014-10-01', '2015-03-06', '2015-05-20', '2015-08-01']
+end = pd.to_datetime(end).values  #, unit='D').values
+dt = end - start
+mid = start + dt/2
+vel = mat['V'][0]*365.0
+err = mat['S'][0]*365.0
+ax.errorbar(mid, vel, xerr=dt/2, yerr=err, c=c, ls='', zorder=3)
+
 # plot GPS velocity
 c = ut.colors[2]
 ts = ut.io.load_data('dgps', 'velocity', 'upstream')['vh'].resample('15T')
@@ -40,4 +54,4 @@ ax.set_ylim(0.0, 800.0)
 fig.autofmt_xdate()
 
 # save
-fig.savefig('sentinel')
+fig.savefig('ts_satvel')
