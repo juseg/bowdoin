393526f4d5f73f477571e9162d1c9800791a8b96
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-08-18 12:50:35 +0200

    Add Greenland gravimetric mass balance grids.

--- figures/data/retrieve-external.sh
+++ figures/data/retrieve-external.sh
@@ -5,17 +5,22 @@ mkdir -p external
 touch external
 cd external
 
-# measures gimp dem
-url=ftp://sidads.colorado.edu/pub/DATASETS/nsidc0645_MEASURES_gimp_dem_v1/30/gimpdem0_4.tif
-fname=${url##*/}
-[ -f $fname ] || wget $url
+# Measures Greenland ice mapping project (GIMP) dem
+orig=ftp://sidads.colorado.edu/pub/DATASETS/nsidc0645_MEASURES_gimp_dem_v1/30/gimpdem0_4.tif
+dest=$(basename $orig)
+[ -f "$dest" ] || wget $orig
 
-# measures velocities
+# Measures Greenland velocities
 root=ftp://n5eil01u.ecs.nsidc.org/SAN/MEASURES/NSIDC-0478.001
 for date in 2000.09.03 2005.12.13 2006.12.18 2007.11.01 2008.12.01
 do
     year=${date:0:4}
-    url=$root/$date/greenland_vel_mosaic500_$((year))_$((year+1)).tif
-    fname=${url##*/}
-    [ -f $fname ] || wget $url
+    orig=$root/$date/greenland_vel_mosaic500_$((year))_$((year+1)).tif
+    dest=$(basename $orig)
+    [ -f "$dest" ] || wget $orig
 done
+
+# Greenland gravimetric mass balance (GMB) grids
+orig=ogive:/scratch_net/ogive/juliens/geodata/icesheets/greenland-gmb/GIS_GMB_grid.nc
+dest=$(basename $orig)
+[ -f "$dest" ] || scp $orig $dest
