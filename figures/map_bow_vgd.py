93ff4220362f24df2612b78dce0457190bc9c1c7
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-10-23 15:12:53 +0200

    Add Landsat velocity and velocity gradient maps.

---
+++ figures/map_bow_vgd.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import util as ut
+import gpxpy
+
+if __name__ == '__main__':
+
+    # projections and map boundaries
+    ll = ccrs.PlateCarree()
+    utm = ccrs.UTM(19)
+    extent = 465e3, 525e3, 8655e3, 8595e3
+    extent = 500e3, 520e3, 8640e3, 8620e3
+
+
+    # initialize figure
+    figw, figh = 60.0, 60.0
+    fig = plt.figure(figsize=(figw/25.4, figh/25.4))
+    ax = fig.add_axes([0, 0, 1, 1], projection=utm)
+    cax = fig.add_axes([5.0/figw, 35.0/figh, 2.5/figw, 20.0/figh])
+    frame = plt.Rectangle((2.5/figw, 32.5/figh), 15.0/figw, 25.0/figh,
+                          ec='k', fc='w', transform=ax.transAxes)
+    ax.set_rasterization_zorder(2.5)
+    ax.set_extent(extent, crs=utm)
+
+    # plot image data
+    filename = 'data/external/S2A_20160808_175915_456_RGB.jpg'
+    data, extent = ut.ma.open_gtif(filename)
+    data = np.moveaxis(data, 0, 2)
+    ax.imshow(data, extent=extent, transform=utm, cmap='Blues')
+
+    # plot velocity gradient
+    filename = 'data/satellite/bowdoin-landsat/16072015_17082015.tif'
+    data, extent = ut.ma.open_gtif(filename)
+    data = np.ma.masked_equal(data, 65535).astype(np.float)
+    rows, cols = data.shape
+    w, e, s, n = extent
+    dx = (e-w)/cols
+    dy = (s-n)/rows
+    grad = ut.ma.slope(data.astype(np.float), dx, dy, smoothing=0.0)
+    cs = ax.imshow(grad, extent=extent, cmap='Reds', vmin=0.0, vmax=0.5, alpha=0.75)
+
+    # add colorbar
+    ax.add_patch(frame)
+    cb = fig.colorbar(cs, cax)
+    cb.set_label('velocity gradient (a$^{-1}$)', labelpad=2)
+
+    # save
+    fig.savefig('map_bow_vgd')
