ac8290f756a1c27176fb6cafe3020e4c36a27899
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-10-24 16:42:11 +0200

    Add Landsat velocity histogram.

---
+++ figures/hist_ls8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+import matplotlib.pyplot as plt
+import util as ut
+
+if __name__ == '__main__':
+
+    # initialize figure
+    fig, ax = plt.subplots()
+
+    # plot histogram
+    filename = 'data/satellite/bowdoin-landsat/16072015_17082015.tif'
+    data, extent = ut.io.open_gtif(filename)
+    ax.hist(data.flatten(), bins=range(0,451,1), ec='none', fc=ut.palette['darkorange'])
+
+    # save
+    fig.savefig('hist_ls8')
