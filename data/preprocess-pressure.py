93f0d69935f3ce6777e5a91dc1f2da16f7d403c5
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-07-25 12:05:24 +0200

    Ignore very low pressure data from 2017-02-04.

--- data/preprocess-pressure.py
+++ data/preprocess-pressure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import datetime
+import numpy as np
 import pandas as pd
 
 loggers = {'downstream': 'drucksens073303',
@@ -20,6 +21,9 @@ for bh, log in loggers.iteritems():
     df = pd.read_csv('original/pressure/%s_final_storage_1.dat' % log,
                      names=columns, index_col='date', date_parser=date_parser,
                      parse_dates={'date': ['year', 'day', 'time']})
+
+    # replace extreme values by nan (including a strange peak on 2017-07-03)
+    df[df<-1000.0] = np.nan
 
     # write csv files
     for var in ['temp', 'wlev']:
