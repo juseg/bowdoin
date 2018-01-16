245a445d891c0f810800e2296ed3ee83345aff7d
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-01-16 13:48:42 +0100

    Set up zooms on water level timeseries.

--- figures/bowtid_wlevseries.py
+++ figures/bowtid_wlevseries.py
@@ -2,24 +2,33 @@
 # coding: utf-8
 
 import util as ut
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 # initialize figure
-fig, ax = ut.pl.subplots_mm(figsize=(85.0, 65.0),
-                            left=12.0, right=1.5, bottom=9.0, top=1.5)
+fig, grid = ut.pl.subplots_mm(figsize=(150.0, 80.0), nrows=1, ncols=2,
+                              sharex=False, sharey=False, wspace=7.5,
+                              left=10.0, right=2.5, bottom=10.0, top=2.5)
 
 # plot tilt unit water level
 p = ut.io.load_bowtid_data('wlev').resample('1H').mean()
-p.plot(ax=ax)
-
-## plot pressure sensor water level
-#for i, bh in enumerate(ut.boreholes):
-#    ts = ut.io.load_data('pressure', 'wlev', bh).resample('1H').mean()
-#    ts = ts.iloc[2:]  # remove the first hour corresponding to drilling
-#    ts.plot(ax=ax, c='k', ls=['--', ':'][i], lw=1.0, alpha=0.75)
+for ax in grid:
+    p.plot(ax=ax, legend=False)
 
 # add label
-ax.set_ylabel('pressure head (m w.e.)')
-ax.legend(ncol=3)
+grid[0].set_ylabel('pressure head (m w.e.)')
+grid[0].legend(ncol=3)
+
+# set up zoom
+x0, x1, y0, y1 = ['20140715', '20141015', 030.0, 390.0]
+x0, x1, y0, y1 = ['20140901', '20141001', 120.0, 160.0]
+#x0, x1, y0, y1 = ['20150101', '20150201', 155.0, 185.0]
+#x0, x1, y0, y1 = ['20150901', '20151015', 160.0, 170.0]
+#x0, x1, y0, y1 = ['20150915', '20151115', 205.0, 235.0]
+#x0, x1, y0, y1 = ['20170315', '20170501', 135.0, 150.0]
+grid[1].set_xlim(x0, x1)
+grid[1].set_ylim(y0, y1)
+mark_inset(grid[0], grid[1], loc1=2, loc2=3, ec='0.5', ls='--')
 
 # save
 ut.pl.savefig(fig)
