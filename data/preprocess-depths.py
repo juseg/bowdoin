e1aa4368e3c8d89bce918d65126779251bf63139
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-01-27 20:32:48 +0100

    Merge thermistor strings depth preprocessing.

--- data/preprocess-depths.py
+++ data/preprocess-depths.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2
 
 import gpxpy
+import numpy as np
 import pandas as pd
 import cartopy.crs as ccrs
 
 
 sensor_holes = dict(pressure=dict(upper='BH2', lower='BH3'),
+                    thstring=dict(upper='BH2', lower='BH3'),
                     tiltunit=dict(upper='BH1', lower='BH3'))
 observ_dates = dict(BH1='2014-07-17 18:07:00',  # assumed
                     BH2='2014-07-17 18:07:00',  # assumed
@@ -106,9 +108,60 @@ def sensor_depths_evol(sensor, uz, lz, ubase, lbase):
     return uz, lz
 
 
+def thstring_depth_init(ubase, lbase):
+    """
+    Return initial temperature sensor depths as data series.
+
+    This is the most confusing part of the data processing. After looking at it
+    many times I could not recover thermistor depths from the deeper strings.
+
+    Measured surfacing cable lenghts
+    * Th-Bowdoin-2: BH1A 2015-07-18 12:30, 19.70 m to 275 m mark
+      (upper)            2016-07-19 11:45, 21.60 m to 275 m mark (+1.90)
+                    BH1B 2015-07-18 12:30, 13.40 m to sensor 7
+                         2016-07-19 11:45, 15.30 m to sensor 7 (+1.90)
+    * Th-Bowdoin-1: BH2A 2015-07-18 14:30, 7.30 m to 250 m mark
+      (lower)            2016-07-19 13:20, 9.70 m to 250 m mark (+2.40)
+                    BH2B 2015-07-18 14:30, 11.25 m to sensor 6
+                         2016-07-19 13:20, 13.65 m to sensor 6 (+2.40)
+
+    Additional information from Conny, incompatible with the above: at the
+    lower borehole, the gap between the deepest sensor of the surface string
+    and the shallowest sensor on the deeper string is 20 m.
+    """
+
+    # ``SMB was measured [...] on Bowdoin Glacier at [...] BH1 (71 m a.s.l.)
+    # from 19 July 2014 to 16 July 2015 [...] SMB on Bowdoin Glacier for
+    # 2014/15 was [...] -1.96 m a-1 at [...] BH1'' (Tsutaki et al., 2016).
+    melt = 1.96
+
+    # upper borehole
+    surf_string = [0.0 - 13.40 - 20.0*i for i in [-6, -5, -4, -3, 0, -2, -1]]
+    deep_string = [275.0 - 19.70 - 20.0*i for i in range(9)]  # surface cable
+    deep_string = [ubase - melt] + [np.nan] * 8  # nans except for the base
+    uz = pd.Series(index=['UT%02d' % (i+1) for i in range(16)],
+                   data=list(deep_string)+list(surf_string))
+
+    # lower borehole
+    surf_string = [-20.0 - 11.25 - 20.0*i for i in range(-6, 1)]
+    deep_string = [250.0 - 7.30 - 20.0*i for i in range(9)]  # surface cable
+    deep_string = [surf_string[0] - 20.0*i for i in range(-8, 1)]  # Conny
+    deep_string = [lbase - melt] + [np.nan] * 8  # nans except for the base
+    lz = pd.Series(index=['LT%02d' % (i+1) for i in range(16)],
+                   data=list(deep_string)+list(surf_string))
+
+    # add melted ice to reconstruct 2014 positions. Although cable lengths
+    # were measured in 2015, the spacing between sensors is relative to 2014
+    # before longitudinal stretching took place.
+    uz += melt
+    lz += melt
+
+    # return as a pandas data series
+    return uz, lz
+
+
 if __name__ == '__main__':
     """Preprocess borehole sensor depths."""
-    # FIXME: include preprocessing of thermistor string depths.
 
     # first get initial sensor depths
     puz, plz = sensor_depths_init('pressure')
@@ -118,12 +171,18 @@ if __name__ == '__main__':
     ubase = max(puz.max(), uuz.max())
     lbase = max(plz.max(), ulz.max())
 
+    # get initial thermistor depths
+    tuz, tlz = thstring_depth_init(ubase, lbase)
+
     # compute borehole thinning
     puz, plz = sensor_depths_evol('pressure', puz, plz, ubase, lbase)
+    tuz, tlz = sensor_depths_evol('thstring', tuz, tlz, ubase, lbase)
     uuz, ulz = sensor_depths_evol('tiltunit', uuz, ulz, ubase, lbase)
 
     # export to csv
     puz.to_csv('processed/bowdoin-pressure-depth-upper.csv', header=True)
     plz.to_csv('processed/bowdoin-pressure-depth-lower.csv', header=True)
+    tuz.to_csv('processed/bowdoin-thstring-depth-upper.csv', header=True)
+    tlz.to_csv('processed/bowdoin-thstring-depth-lower.csv', header=True)
     uuz.to_csv('processed/bowdoin-tiltunit-depth-upper.csv', header=True)
     ulz.to_csv('processed/bowdoin-tiltunit-depth-lower.csv', header=True)
--- data/preprocess-thstring.py
+++ data/preprocess-thstring.py
@@ -41,63 +41,12 @@ def get_temperature(bh, log, manual=False):
     return df
 
 
-def get_depth(bh):
-    """Return temperature sensor depths in a data series."""
-
-    # Measured surfacing cable lenghts
-    # * Th-Bowdoin-2: BH1A 2015-07-18 12:30, 19.70 m to 275 m mark
-    #   (upper)            2016-07-19 11:45, 21.60 m to 275 m mark
-    #                 BH1B 2015-07-18 12:30, 13.40 m to sensor 7
-    #                      2016-07-19 11:45, 15.30 m to sensor 7
-    #
-    # * Th-Bowdoin-1: BH2A 2015-07-18 14:30, 7.30 m to 250 m mark
-    #   (lower)            2016-07-19 13:20, 9.70 m to 250 m mark
-    #                 BH2B 2015-07-18 14:30, 11.25 m to sensor 6
-    #                      2016-07-19 13:20, 13.65 m to sensor 6
-
-    # calculate sensor depths
-    if bh == 'lower':
-        date = '2015-07-18 14:30'
-        bottom = 7.30 - 250.0
-        top =  11.25 + 20.0
-        lower = bottom + 20.0*np.arange(9)
-        upper = top + 20.0*np.arange(-6, 1)
-        gap = upper[0] - lower[-1]
-        lower += gap - 20.0 # according to conny
-    if bh == 'upper':
-        date = '2015-07-18 12:30'
-        bottom = 19.70 - 275.0
-        top =  13.40 + 0.0
-        lower = bottom + 20.0*np.arange(9)
-        upper = top + 20.0*np.array([-6, -5, -4, -3, 0, -2, -1])
-
-    # lower depths make no sense; mark as unknown
-    lower[:] = np.nan
-    depth = -np.hstack([lower, upper])
-
-    # except for the lowest sensor, assumed to be at the base
-    # ``SMB was measured [...] on Bowdoin Glacier at [...] BH1 (71 m a.s.l.)
-    # from 19 July 2014 to 16 July 2015 [...] SMB on Bowdoin Glacier for
-    # 2014/15 was [...] -1.96 m a-1 at [...] BH1'' (Tsutaki et al., 2016).
-    melt = 1.96
-    filename = 'processed/bowdoin-pressure-depth-%s.csv' % bh
-    base = pd.read_csv(filename, parse_dates=True, index_col='date', squeeze=True)[0]
-    depth[0] = base - melt
-
-    # return as a pandas data series
-    columns = [bh[0].upper() + 'T%02d' % (i+1) for i in range(16)]
-    df = pd.DataFrame(index=[date], columns=columns, data=[depth])
-    df.index = df.index.rename('date')
-    return df
-
-
 # for each borehole
 for bh, log in loggers.iteritems():
 
     # compute sensor depths
     filename = 'processed/bowdoin-thstring-depth-%s.csv' % bh
-    depth = get_depth(bh)
-    depth.to_csv(filename)
+    depth = pd.read_csv(filename, index_col='date')
 
     # preprocess data logger temperatures
     # FIXME: depth was measured in 2015, temp in 2014
