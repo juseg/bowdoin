d71358c51f8a2a79ec2ecc4f8a722581c01887fb
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-02-03 13:56:29 +0100

    Merge preprocessing of differential gps data.

--- data/preprocess-boreholes.py
+++ data/preprocess-boreholes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import os
 import gpxpy
 import numpy as np
 import pandas as pd
@@ -19,6 +20,8 @@ observ_dates = dict(BH1='2014-07-17 18:07:00',  # assumed
 water_depths = dict(BH1=48.0, BH2=46.0, BH3=0.0)
 
 # data logger properties
+dgps_columns = ['daydate', 'time', 'lat', 'lon', 'z', 'Q', 'ns',
+                'sdn', 'sde', 'sdu', 'sdne', 'sdeu', 'sdun', 'age', 'ratio']
 pressure_loggers = dict(lower='drucksens073303', upper='drucksens094419')
 pressure_columns = ['id', 'year', 'day', 'time', 'temp', 'pres', 'wlev']
 tiltunit_loggers = dict(lower='BOWDOIN-1', upper='BOWDOIN-2')
@@ -200,6 +203,57 @@ def thstring_depth_init(ubase, lbase):
 # Data reading methods
 # --------------------
 
+def get_dgps_data(method='backward'):
+    """Return lon/lat gps positions in a data frame."""
+
+    # check argument validity
+    assert method in ('backward', 'forward', 'central')
+
+    # append dataframes corresponding to each year
+    dflist = []
+    df = pd.concat([pd.read_fwf('original/gps/B14BH1/B14BH1_%d_15min.dat' % year,
+                                names=dgps_columns, index_col=0,
+                                usecols=['daydate', 'time', 'lon', 'lat', 'z'],
+                                parse_dates={'date': ['daydate', 'time']})
+                    for year in [2014, 2015, 2016, 2017]])
+
+    # find samples not taken at multiples of 15 min (900 sec) and remove them
+    # it seems that these (18) values were recorded directly after each data gap
+    inpace = (60*df.index.minute + df.index.second) % 900 == 0
+    assert (not inpace.sum() < 20)  # make sure we remove less than 20 values
+    df = df[inpace]
+
+    # convert lon/lat to UTM 19 meters
+    ll = ccrs.PlateCarree()
+    proj = ccrs.UTM(19)
+    points = proj.transform_points(ll, df['lon'].values, df['lat'].values)
+    df['x'] = points[:,0]
+    df['y'] = points[:,1]
+
+    # resample with 15 minute frequency and fill with NaN
+    df = df.resample('15T').mean()
+
+    # compute cartesian velocity in meters per year
+    v = df[['x', 'y', 'z']]
+    if method == 'backward':
+        v = v.diff()
+    elif method == 'forward':
+        v = -v.diff(-1)
+    elif method == 'central':
+        v = (v.diff()-v.diff(-1))/2.0
+    v *= 60*24*365/15.0
+    v.columns = ['vx', 'vy', 'vz']
+    df = df.join(v)
+
+    # compute velocity polar coordinates
+    df['vh'] = (df['vx']**2 + df['vy']**2)**0.5
+    df['azimuth'] = np.arctan2(df['vy'], df['vx']**2)*180/np.pi
+    df['altitude'] = np.arctan2(df['vz'], df['vh'])*180/np.pi
+
+    # return complete dataframe
+    return df
+
+
 def get_pressure_data(bh, log):
     """Return pressure sensor data in a data frame."""
 
@@ -337,6 +391,16 @@ def cal_temperature(temp, depth, t0='2014-07-23 11:20', t1='2014-07-23 15:00'):
 
 if __name__ == '__main__':
     """Preprocess borehole data."""
+
+    # make directory or update modification date
+    if os.path.isdir('processed'):
+        os.utime('processed', None)
+    else:
+        os.mkdir('processed')
+
+    # preprocess gps data
+    vdf = get_dgps_data()
+    vdf.to_csv('processed/bowdoin-dgps-velocity-upper.csv')
 
     # read all data
     pudf = get_pressure_data('upper', pressure_loggers['upper'])
