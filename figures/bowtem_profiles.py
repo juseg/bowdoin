ecafb52935fdaa2e4772b127e939b44ab41c8c21
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-02-26 15:50:33 +0100

    Update bowtem temperature profiles.

--- figures/bowtem_profiles.py
+++ figures/bowtem_profiles.py
@@ -1,84 +1,56 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import util as ut
 
 
 # dates to plot
-dates = dict(upper=['2015-01-01', '2016-07-12'],
-             lower=['2015-01-01', '2015-11-12', '2016-07-19'])
-styles = ['-', ':', ':']
-
-# markers per sensor type
-markers = dict(T='o', I='^')  #, pres='s')
+dates = dict(
+    bh1=['2014-10-25', '2015-10-14', '2017-01-28'],
+    bh2=['2014-11-01', '2015-07-14'],
+    bh3=['2015-01-01', '2015-04-01', '2015-07-12', '2015-11-12', '2016-06-19'])
 
 # initialize figure
-fig, ax = plt.subplots()
+fig, ax = ut.pl.subplots_mm(figsize=(150.0, 75.0), nrows=1, ncols=1,
+                            sharex=False, sharey=True, wspace=2.5,
+                            left=10.0, right=2.5, bottom=10.0, top=2.5)
 
-# for each borehole
-base_depth = 0.0
-for i, bh in enumerate(ut.boreholes):
-    c = ut.colors[bh]
+# loop on boreholes
+for bh, c in zip(ut.bowtem_bhnames, ut.bowtem_colours):
 
-    # read temperature and depth data
-    temp_depth = ut.io.load_depth('thstring', bh)
-    tilt_depth = ut.io.load_depth('tiltunit', bh)
-    pres_depth = ut.io.load_depth('pressure', bh)
-    pres_depth.index = ['pres']
-    manu_temp = ut.io.load_data('thstring', 'mantemp', bh)
-    temp_temp = ut.io.load_data('thstring', 'temp', bh)
-    tilt_temp = ut.io.load_data('tiltunit', 'temp', bh)
+    # load data
+    t, z, b = ut.io.load_bowtem_data(bh)
 
-    # prepare depth profile
-    depth = pd.concat((temp_depth, tilt_depth))
-    subglac = depth > 0.0
-    notnull = depth.notnull() #& temp.notnull()
-    depth = depth[notnull&subglac].sort_values()
+    # plot first date
+    d0 = dates[bh][0]
+    t0 = t[d0].mean()
+    ax.plot(t0, z, c=c, label='%s, %s' % (bh.upper(), d0))
+    for s, m in zip(ut.bowtem_sensors, ut.bowtem_markers):
+        cols = z.index.str[1] == s
+        if cols.sum() > 0:
+            ax.plot(t0[cols], z[cols], c=c, marker=m, ls='', label='')
 
-    # for each date to plot
-    for date, ls in zip(dates[bh], styles):
+    # plot next dates
+    for d in dates[bh][1:]:
+        ax.plot(t[d].mean(), z, c=c, ls='--', label='')
 
-        # prepare combined profiles
-        if date in temp_temp.index:
-            temp = temp_temp[date].mean()
-        else:
-            temp = manu_temp[date].squeeze()
-        if date in tilt_temp.index:
-            temp = temp.append(tilt_temp[date].mean())
-
-        # order by depth, remove nulls and sensors above ground
-        temp = temp[depth.index.values]
-
-        # plot profiles
-        label = '{}, {}'.format(bh, date)
-        ax.plot(temp, depth, c=ut.colors[bh], ls=ls, label=label)
-        if date == dates[bh][0]:
-            for sensor, marker in markers.iteritems():
-                cols = [s for s in temp.index if s[1] == sensor]
-                #ax.plot(temp[cols], depth[cols], marker=marker, c=ut.colors[bh])
-
-    # add base line  # FIXME UI02 is lower than that
-    #ax.plot([temp['temp01']-0.5, temp['temp01']+0.5],
-    #        [depth['temp01'], depth['temp01']], c='k')
-
-    # compute maximum depth
-    base_depth = max(base_depth, pres_depth.squeeze())
+    # add base line
+    ax.axhline(b, c=c, lw=0.5)
 
 # plot melting point
+b = 272.0       # base
 g = 9.80665     # gravity
 rhoi = 910.0    # ice density
 beta = 7.9e-8   # Luethi et al. (2002)
-base_temp_melt = -beta * rhoi * g * base_depth
-ax.plot([0.0, base_temp_melt], [0.0, base_depth], c='k', ls=':')
+melt = -beta * rhoi * g * b
+ax.plot([0.0, melt], [0.0, b], c='k', ls=':')
 
 # set axes properties
-ax.invert_yaxis()
-ax.set_xlabel(u'ice temperature (°C)')
-ax.set_ylabel('depth (m)')
 ax.legend(loc='lower left')
+ax.set_xlabel(u'ice temperature (°C)')
+ax.set_ylabel('initial depth (m)')
+ax.axhline(0.0, c='k', lw=0.5)
+ax.invert_yaxis()
 
 # save
 ut.pl.savefig(fig)
