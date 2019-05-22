852b78912221a4e320cf09e4e4c2d4b81d0066a8
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-05-22 12:03:04 +0200

    Completely rewrite borehole figure with new utils.

--- figures/bowtem_boreholes.py
+++ figures/bowtem_boreholes.py
@@ -1,104 +1,120 @@
 #!/usr/bin/env python
+# Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-import util as ut
-import numpy as np
+"""Plot Bowdoin temperature borehole setup."""
+
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import cartowik.shadedrelief as csr
+import absplots as apl
+import util
 
 
-# Initialize figure
-# -----------------
+def init_figure():
+    """Initialize figure with map and profile subplots."""
 
-# projections and map boundaries
-ll = ccrs.PlateCarree()
-utm = ccrs.UTM(19)
-reg = 507.5e3, 512.5e3, 8620e3, 8627e3  # 5.0x7.0 km full glacier tongue
-reg = 507.6e3, 512.4e3, 8620.7e3, 8626.3e3  # 4.8x5.6 km full glacier tongue
+    # initialize figure
+    # FIXME implement add_axes_mm / add_subplot_mm for unique subplots
+    fig = apl.figure_mm(figsize=(150, 75))
+    gridspec_kw = dict(left=2.5, right=87.5, bottom=2.5, top=2.5)
+    ax0 = fig.subplots_mm(gridspec_kw=gridspec_kw,
+                          subplot_kw=dict(projection=ccrs.UTM(19)))
+    gridspec_kw = dict(left=75, right=2.5, bottom=10, top=2.5)
+    ax1 = fig.subplots_mm(gridspec_kw=gridspec_kw)
 
-# initialize figure
-figw, figh = 150.0, 75.0  # FIXME implement add_axes_mm / add_subplot_mm
-fig = plt.figure(figsize=(figw/25.4, figh/25.4))
-ax1 = fig.add_axes([2.5/figw, 2.5/figh, 60.0/figw, 1-5.0/figh], projection=utm)
-ax2 = fig.add_axes([75.0/figw, 10.0/figh, 72.5/figw, 1-12.5/figh])
+    # add subfigure labels
+    util.com.add_subfig_label(ax=ax0, text='(a)', color='w')
+    util.com.add_subfig_label(ax=ax1, text='(b)')
 
-# add subfigure labels
-ut.pl.add_subfig_label(ax=ax1, text='(a)', ha='right')
-ut.pl.add_subfig_label(ax=ax2, text='(b)')
-
-
-# Map axes
-# --------
-
-ax = ax1
-
-# initialize figure
-ax.set_rasterization_zorder(2.5)
-ax.set_extent(reg, crs=utm)
-
-# plot image data
-filename = '../data/external/S2A_20160808_175915_456_RGB.jpg'
-data, extent = ut.io.open_gtif(filename)
-data = np.moveaxis(data, 0, 2)
-ax.imshow(data, extent=extent, transform=utm, cmap='Blues')
-
-# plot borehole locations
-for bh, c in zip(ut.bowtem_bhnames, ut.bowtem_colours):
-    for y in ['14', '16', '17']:
-        ut.pl.add_waypoint('B'+y+bh.upper(), text='20'+y, ax=ax, color=c,
-                           marker='o', textpos=('lr' if bh == 'bh1' else 'ul'))
-
-# plot camp location
-ut.pl.add_waypoint('Tent Swiss', ax=ax, color='w', marker='^',
-                   text='Camp', textpos='lc')
-
-# add scale
-ut.pl.add_scale(ax=ax, length=1000, label='1km', color='w')
+    # return figure and axes
+    return fig, (ax0, ax1)
 
 
-# Distance axes
-# -------------
+def plot_location_map(ax):
+    """Draw boreholes location map with Sentinel image background."""
 
-ax = ax2
+    # prepare map axes
+    ax.set_rasterization_zorder(2.5)
+    ax.set_extent([507.6e3, 512.4e3, 8620.7e3, 8626.3e3], crs=ax.projection)
 
-# borehole plot properties
-distdict = dict(bh1=2.015, bh2=1.985, bh3=1.85)
-markdict = dict(zip(ut.bowtem_sensors, ut.bowtem_markers))
-xoffdict = dict(zip(ut.bowtem_sensors, [0.01, -0.01, -0.01]))
+    # plot Sentinel image data
+    filename = '../data/external/S2A_20160808_175915_456_RGB.jpg'
+    # FIXME: Cartowik.shadedrelief._open_raster_data is a private method.
+    # FIXME: Make it public? Or add a method to plot RGB images?
+    # FIXME: Besiodes band=1 is a poor default for RGB images.
+    from rasterio.plot import reshape_as_image
+    data, extent = csr._open_raster_data(filename, band=None)
+    data = reshape_as_image(data)
+    ax.imshow(data, extent=extent, origin='upper', cmap='Blues')
 
-# loop on boreholes
-for bh, c in zip(ut.bowtem_bhnames, ut.bowtem_colours):
-    t, z, b = ut.io.load_bowtem_data(bh)
-    d = distdict[bh]
-    for u in z.index:
-        m = markdict[u[1]]
-        x = xoffdict[u[1]]
-        ax.plot(d+1*x, z[u], c=c, marker=m, ls='', label='')
-        ax.text(d+2*x, z[u], u,
-                ha=('left' if x>0 else 'right'),
-                va=('bottom' if u in ('LP') else
-                    'top' if u in ('LT01', 'UT01') else
-                    'center'))
-    ax.plot([d, d], [b, 0.0], 'k-_')
-    ax.text(d, -5.0, bh.upper(), ha='center', va='bottom')
-    ax.text(d, b+5.0, '%d m' % b, ha='center', va='top')
+    # add boreholes and camp waypointsfor each borehole
+    # FIXME: this GPX interface could also be a part of cartowik
+    for bh, color in util.tem.COLOURS.items():
+        for y in ['14', '16', '17']:
+            util.geo.add_waypoint('B'+y+bh.upper(), ax=ax, color=color,
+                                  marker='o', text='20'+y,
+                                  textpos=('lr' if bh == 'bh1' else 'ul'))
+    util.geo.add_waypoint('Tent Swiss', ax=ax, color='w', marker='^',
+                          text='Camp', textpos='lc')
 
-# add flow direction arrow
-ax.text(0.9, 0.55, 'ice flow', ha='center', transform=ax.transAxes)
-ax.annotate('', xy=(0.85, 0.5), xytext=(0.95, 0.5),
-            xycoords=ax.transAxes, textcoords=ax.transAxes,
-            arrowprops=dict(arrowstyle='->',  lw=1.0))
+    # add scale
+    util.geo.add_scale_bar(ax=ax, length=1000, label='1km', color='w')
 
-# add standalone legend
-ax.legend([plt.Line2D([], [], ls='none', marker=m) for m in ut.bowtem_markers],
-          ['Inclinometers', 'Thermistors', 'Piezometres'],
-          bbox_to_anchor=(1.0, 0.95))
 
-# set axes properties
-ax.set_xlim(1.75, 2.15)
-ax.set_ylim(292.0, -20.0)
-ax.set_xticks([1.85, 2.0])
-ax.set_xlabel('approximate distance from front in 2014 (km)')
-ax.set_ylabel('depth (m)')
+def plot_long_profile(ax):
+    """Draw boreholes long profile with intrumental setup."""
 
-# save
-ut.pl.savefig(fig)
+    # borehole plot properties
+    distances = dict(bh1=2.015, bh2=1.985, bh3=1.85)
+
+    # for each borehole
+    for bh, color in util.tem.COLOURS.items():
+        temp, dept, base = util.tem.load_all(bh)
+        dist = distances[bh]
+        for unit, dept in dept.items():
+            sensor = unit[1]
+            marker = util.tem.MARKERS[sensor]
+            offset = 0.01 if sensor == 'I' else -0.01
+            ax.plot(dist+1*offset, dept, color=color, marker=marker,
+                    label='', ls='')
+            ax.text(dist+2*offset, dept, unit,
+                    ha=('left' if offset > 0 else 'right'),
+                    va=('bottom' if unit in ('LP') else
+                        'top' if unit in ('LT01', 'UT01') else
+                        'center'))
+        ax.plot([dist, dist], [base, 0.0], 'k-_')
+        ax.text(dist, -5.0, bh.upper(), ha='center', va='bottom')
+        ax.text(dist, base+5.0, '%d m' % base, ha='center', va='top')
+
+    # add flow direction arrow
+    ax.text(0.9, 0.55, 'ice flow', ha='center', transform=ax.transAxes)
+    ax.annotate('', xy=(0.85, 0.5), xytext=(0.95, 0.5),
+                xycoords=ax.transAxes, textcoords=ax.transAxes,
+                arrowprops=dict(arrowstyle='->', lw=1.0))
+
+    # add standalone legend
+    labels = ['Inclinometers', 'Thermistors', 'Piezometres']
+    markers = [util.tem.MARKERS[l[0]] for l in labels]
+    handles = [plt.Line2D([], [], ls='none', marker=m) for m in markers]
+    ax.legend(handles, labels, bbox_to_anchor=(1.0, 0.90))
+
+    # set axes properties
+    ax.set_xlim(1.75, 2.15)
+    ax.set_ylim(292, -20)
+    ax.set_xticks([1.85, 2.0])
+    ax.set_xlabel('approximate distance from front in 2014 (km)')
+    ax.set_ylabel('depth (m)')
+
+
+def main():
+    """Main program called during execution."""
+    fig, (ax0, ax1) = init_figure()
+    plot_location_map(ax0)
+    plot_long_profile(ax1)
+    util.com.savefig(fig)
+
+
+if __name__ == '__main__':
+    main()
