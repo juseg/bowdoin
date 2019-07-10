0ee88021afb4ed233b1572ba51ecc59f72302577
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-07-10 15:21:09 +0200

    Add temperature calibration curves.

---
+++ figures/bowtem_calibration.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
+
+"""Plot Bowdoin temperature calibration curves."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import absplots as apl
+import util
+
+
+def main():
+    """Main program called during execution."""
+
+    # initialize figure
+    fig, ax = apl.subplots_mm(figsize=(150, 75), gridspec_kw=dict(
+        left=10, right=2.5, bottom=10, top=2.5))
+
+    # for each borehole
+    for bh in ['bh1', 'bh3']:
+        cmap = dict(bh1='Blues', bh3='Greens')[bh]
+        site = dict(bh1='U', bh3='L')[bh]
+        logger = dict(bh1='Th-Bowdoin-2', bh3='Th-Bowdoin-1')[bh]
+
+        # input file name
+        cfilename = '../data/original/temperature/%s_Coefs.dat' % logger
+
+        # read rearranged calibration coefficients
+        # sensor order lower: BH2A[1-9] + BH2B[1-7],
+        #              upper: BH1A[1-9] + BH1B[1-4,7,5-6].
+        coefs = np.loadtxt(cfilename)
+        cmap = plt.get_cmap(cmap, coefs.shape[0])
+
+        # compute temperature from resistance
+        res = np.linspace(20, 40, 101)
+        for i, (cf1, cf2, cf3) in enumerate(coefs):
+            temp = 1 / (cf1 + cf2*np.log(res) + cf3*np.log(res)**3) - 273.15
+            ax.plot(res, temp, color=cmap(i), lw=0.5,
+                    label=site+'T{:02d}'.format(i+1))
+
+    # set axes properties
+    ax.set_xlabel(r'resistance ($k\Omega$)')
+    ax.set_ylabel(u'temperature (°C)')
+    ax.legend(ncol=4)
+
+    # save
+    util.com.savefig(fig)
+
+
+if __name__ == '__main__':
+    main()
