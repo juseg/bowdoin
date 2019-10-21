7e1ca84599b80397a0d41dafa6230933536359f2
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-10-21 14:49:10 +0200

    Use brokenaxes on timeseries fig to show trend.

--- figures/bowtem_timeseries.py
+++ figures/bowtem_timeseries.py
@@ -6,16 +6,30 @@
 """Plot Bowdoin temperature time series."""
 
 import matplotlib.pyplot as plt
+import pandas as pd
+import brokenaxes as bax
 import absplots as apl
 import util
+
+# Pandas plot methods do not work on brokenaxes (issue #40)
+pd.plotting.register_matplotlib_converters()
 
 
 def main():
     """Main program called during execution."""
 
+    # FIXME implement gridspec_mm in absplots
+    # fig = apl.figure_mm(figsize=(180, 120))
+    # subplotspec = fig.add_gridspec_mm(1, 1)[:]
+
     # initialize figure
     gridspec_kw = dict(left=12.5, right=2.5, bottom=12.5, top=2.5)
-    fig, ax = apl.subplots_mm(figsize=(180, 90), gridspec_kw=gridspec_kw)
+    fig, ax = apl.subplots_mm(figsize=(180, 120), gridspec_kw=gridspec_kw)
+    subplotspec = ax.get_subplotspec()
+    ax.remove()
+    ax = bax.brokenaxes(despine=False, height_ratios=[4, 1], hspace=0.02,
+                        subplot_spec=subplotspec, tilt=15,
+                        ylims=[(-15.5, -6.5), (-6.5, 0.5)])
 
     # for each borehole
     for bh, color in util.tem.COLOURS.items():
@@ -23,31 +37,42 @@ def main():
         # plot daily means
         temp, _, _ = util.tem.load_all(bh)
         temp = temp.resample('1D').mean()
-        temp.plot(ax=ax, c=color, legend=False, lw=0.5)
+        ax.plot(temp.index, temp.values, c=color, lw=0.5)
+        # temp.plot(ax=ax, c=color, legend=False, lw=0.5)  # fails (issue #40)
 
         # plot manual readings
         if bh != 'bh1':
             manu, mask = util.tem.load_manual(bh)
             manu = manu.resample('1D').mean()
             mask = mask.resample('1D').prod()
-            manu.where(mask).plot(ax=ax, c='none', marker='o', mec=color)
-            manu.mask(mask).plot(ax=ax, c=color, marker='o')
+            ax.plot(manu.where(mask).index, manu.where(mask).values,
+                    c='none', marker='o', mec=color)
+            ax.plot(manu.mask(mask).index, manu.mask(mask).values,
+                    c=color, marker='o')
+            # manu.where(mask).plot(ax=ax, c='none', marker='o', mec=color)
+            # manu.mask(mask).plot(ax=ax, c=color, marker='o')  # fails (#40)
 
         # add profile dates
         for date in util.tem.PROFILES_DATES[bh]:
             ax.axvline(date, color=color, ls='--')
 
     # add campaigns
-    util.com.plot_field_campaigns(ax=ax)
+    util.com.plot_field_campaigns(ax=ax.axs[0], ytext=-1)
+    util.com.plot_field_campaigns(ax=ax.axs[1])
 
     # set axes properties
-    ax.set_ylabel(u'temperature (°C)')
+    ax.set_xlabel('date', labelpad=24)
+    ax.set_ylabel(u'temperature (°C)', labelpad=24)
     ax.set_xlim('20140615', '20170815')
-    ax.set_ylim(-14.5, 0.5)
+
+    # set better ticks
+    ax.axs[0].set_yticks(range(-6, 1))
+    ax.axs[1].set_yticks(range(-14, -6, 2))
 
     # add standalone legend
-    ax.legend(*zip(*[(plt.Line2D([], [], c=c, marker='o'*(bh != 'bh1')),
-                      bh.upper()) for bh, c in util.tem.COLOURS.items()]))
+    ax.axs[0].legend(*zip(*[
+        (plt.Line2D([], [], c=c, marker='o'*(bh != 'bh1')),
+         bh.upper()) for bh, c in util.tem.COLOURS.items()]))
 
     # save
     util.com.savefig(fig)
