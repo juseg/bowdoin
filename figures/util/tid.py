412c3af5ae01e20bfc1e99490f14aa18b79b99ba
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-10-29 17:16:49 +0100

    Cleanup tide loading methods with pylint.

--- figures/util/tid.py
+++ figures/util/tid.py
@@ -9,37 +9,38 @@ Bowdoin tides paper utils.
 import scipy.signal as sg
 import pandas as pd
 
-# physical constants
-g = 9.80665  # gravitational acceleration in m s-2
-rhos = 1029.0  # sea water density in kg m-3
+# Physical constants
+# ------------------
+
+SEA_DENSITY = 1029      # Sea wat. density,     kg m-3          (--)
+GRAVITY = 9.80665       # Standard gravity,     m s-2           (--)
 
 
 # Data loading methods
 # --------------------
 
-def has_two_lines(fname):
-    """Check if given file has more than one line."""
-    with open(fname) as f:
-        l1 = f.readline()
-        l2 = f.readline()
-    result = l2 != ''
-    return result
+def is_multiline(filename):
+    """Return True if file has at least two lines."""
+    with open(filename) as fil:
+        line = fil.readline()
+        line = fil.readline()
+    return line != ''
 
 
 def load_bowdoin_tides(order=2, cutoff=1/3600.0):
     """Return Masahiro filtered sea level in a data series."""
 
     # open postprocessed data series
-    ts = pd.read_csv('../data/processed/bowdoin.tide.csv', index_col=0,
-                     parse_dates=True, squeeze=True)
+    tide = pd.read_csv('../data/processed/bowdoin.tide.csv', index_col=0,
+                       parse_dates=True, squeeze=True)
 
     # apply two-way lowpass filter
-    ts = ts.asfreq('2s').interpolate()
-    b, a = sg.butter(order, 2*cutoff, 'low')  # order, cutoff freq
-    ts[:] = sg.filtfilt(b, a, ts)
+    tide = tide.asfreq('2s').interpolate()
+    filt = sg.butter(order, cutoff, 'low')
+    tide[:] = sg.filtfilt(*filt, tide)
 
     # return pressure data series
-    return ts
+    return tide
 
 
 def load_pituffik_tides(start='2014-07', end='2017-08'):
@@ -48,14 +49,14 @@ def load_pituffik_tides(start='2014-07', end='2017-08'):
     # find non-tempy data files
     dates = pd.date_range(start=start, end=end, freq='M')
     files = dates.strftime('../data/external/tide-thul-%Y%m.csv')
-    files = [f for f in files if has_two_lines(f)]
+    files = [f for f in files if is_multiline(f)]
 
     # open in a data series
     csvkw = dict(index_col=0, parse_dates=True, header=1, squeeze=True)
-    ts = pd.concat([pd.read_csv(f, **csvkw) for f in files])
+    series = pd.concat([pd.read_csv(f, **csvkw) for f in files])
 
     # convert tide (m) to pressure (kPa)
-    ts = 1e-3*rhos*g*(ts-ts.mean())
+    series = 1e-3 * SEA_DENSITY * GRAVITY * (series-series.mean())
 
     # return pressure data series
-    return ts
+    return series
