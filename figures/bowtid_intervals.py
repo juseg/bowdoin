f1da1a0abb45a4e9a2a3d11cd485eea6ca304bd7
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-10-30 10:57:32 +0100

    Cleanup interval series and add to Makefile.

--- figures/bowtid_intervals.py
+++ figures/bowtid_intervals.py
@@ -3,41 +3,44 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-import util
+"""Plot Bowdoin tides time intervals time series."""
+
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 import absplots as apl
+import util
 
 
 def main():
     """Main program called during execution."""
 
     # initialize figure
-    fig, grid = apl.subplots_mm(figsize=(180, 90), nrows=9, ncols=2,
-        sharex='col', sharey=True, gridspec_kw=dict(
-            left=12.5, right=2.5, bottom=12.5, top=2.5, hspace=2.5, wspace=12.5))
+    fig, grid = apl.subplots_mm(
+        figsize=(180, 90), nrows=9, ncols=2, sharex='col', sharey=True,
+        gridspec_kw=dict(left=12.5, right=2.5, wspace=12.5,
+                         bottom=12.5, top=2.5, hspace=2.5))
 
     # for each tilt unit
-    p = util.tid.load_inc('wlev')
-    for i, u in enumerate(p):
-        c = 'C%d' % i
+    pres = util.tid.load_inc('wlev')
+    for i, unit in enumerate(pres):
+        color = 'C%d' % i
 
         # extract time steps
-        ts = p[u].dropna()
-        ts = ts.index.to_series().diff().dt.total_seconds()/3600.0
-        ts = ts[1:].resample('1H').mean()  # resample to get a nice date axis
+        step = pres[unit].dropna()
+        step = step.index.to_series().diff().dt.total_seconds()/3600.0
+        step = step[1:].resample('1H').mean()  # resample to get nice date axis
 
         # plot
         for ax in grid[i]:
-            ts.plot(ax=ax, c=c)
+            step.plot(ax=ax, c=color)
 
         # add corner tag
-        grid[i, 0].text(0.95, 0.2, u, color=c, transform=grid[i, 0].transAxes)
+        grid[i, 0].text(0.9, 0.2, unit, color=color,
+                        transform=grid[i, 0].transAxes)
         grid[i, 0].set_yticks([0, 2])
         grid[i, 0].set_ylim(-0.5, 2.5)
 
         # set up zoom
-        x0, x1 = ['20141101', '20141201']
-        grid[i, 1].set_xlim(x0, x1)
+        grid[i, 1].set_xlim('20141101', '20141201')
         mark_inset(grid[i, 0], grid[i, 1], loc1=2, loc2=3, ec='0.5', ls='--')
 
     # set y label
