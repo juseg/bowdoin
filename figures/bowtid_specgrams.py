00dfd8f5c5a1f1a641dd93845727753d34a35505
Julien Seguinot <juseg@posteo.eu>
2021-02-18 14:26:06 +0100

    Fix spectrograms, argh took me hours...

--- figures/bowtid_specgrams.py
+++ figures/bowtid_specgrams.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
-# Copyright (c) 2019, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2019-2021, Julien Seguinot (juseg.github.io)
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
 import util
-import numpy as np
 import pandas as pd
-import scipy.signal as sg
-import matplotlib.colors as mcolors
+import matplotlib.dates as mdates
 import absplots as apl
 
 
 def main():
     """Main program called during execution."""
 
-
     # initialize figure
-    fig, grid = apl.subplots_mm(figsize=(180, 90), nrows=10, ncols=1,
+    fig, grid = apl.subplots_mm(
+        figsize=(180, 90), nrows=10, sharex=True, sharey=True,
         gridspec_kw=dict(
-            left=12.5, right=15, bottom=12.5, top=2.5, hspace=2.5))
-    cax = fig.add_axes_mm([162.5, 10, 2.5, 77.5])
+            left=12.5, right=20, bottom=12.5, top=2.5, hspace=2.5))
+    cax = fig.add_axes_mm([180-17.5, 12.5, 2.5, 90-15])
 
     # get freezing dates
     t = util.tid.load_inc('temp')['20140717':].resample('1H').mean()
@@ -27,61 +25,60 @@ def main():
 
     # for each tilt unit
     p = util.tid.load_inc('wlev')
-    norm = mcolors.LogNorm(1e-6, 1e0)
     for i, u in enumerate(p):
-        ax = grid.flat[i]
+        ax = grid[i]
         c = 'C%d' % i
 
         # crop, resample, and interpolate
-        ts = p[u][df[u]:].dropna().resample('1H').mean().interpolate().diff()[1:]/3.6
-        ts.plot(ax=ax, visible=False)  # prepare axes in pandas format
+        ts = p[u][df[u]:].dropna().resample('1H').mean()
+        ts = ts.interpolate().diff()[1:]/3.6
 
         # calculate sample frequency
         dt = (ts.index[1]-ts.index[0])/pd.to_timedelta('1H')
         fs = 1 / dt
 
-        # compute spectrogram and corresponding time coordinate
-        nfft = 8*24
-        f, t, spec = sg.spectrogram(ts, nperseg=nfft, fs=fs, noverlap=0,
-                                    scaling='spectrum')
-        freq = '%dH' % (nfft/fs)
-        periods = spec.shape[1]
-        t = (pd.date_range(ts.index[0], freq=freq, periods=periods) +
-             pd.to_timedelta(freq)/2.0).to_pydatetime()
+        # ensure same kwargs with last (tide) plot
+        kwargs = dict(
+            Fs=fs, NFFT=24*8, noverlap=0,
+            cmap='Greys', interpolation='nearest', vmin=-40, vmax=20)
 
-        ## log-scale amplitude values
-        #gain = 10.0*np.log10(spec)
-
-        # plot amplitudes
-        im = ax.pcolormesh(t, 1/f[1:], spec[1:], cmap='Greys', norm=norm)
+        # plot spectrogram
+        _, _, _, im = ax.specgram(
+            ts, xextent=mdates.date2num([ts.index[0], ts.index[-1]]), **kwargs)
 
         # add corner tag
         ax.text(0.95, 0.2, u, color=c, transform=ax.transAxes,
                 bbox=dict(ec='none', fc='w', alpha=0.75, pad=1))
 
         # set axes properties
-        ax.set_xlim(p.index[0], p.index[-1])
-        #ax.set_yscale('log')
-        ax.set_ylim(6.0, 30.0)
-        ax.set_yticks([12.0, 24.0])
+        ax.set_ylim(0.5/24, 2.5/24)
+        ax.set_yticks([1/24, 1/12])
+        ax.set_yticklabels(['24', '12'])
 
     # plot tide data
     ax = grid.flat[-1]
-    ts = ut.io.util.tid.load_pituffik_tides().resample('1H').mean().interpolate().diff()[1:]/3.6
-    ts.plot(ax=ax, visible=False)  # prepare axes in pandas format
-    f, t, spec = sg.spectrogram(ts, nperseg=nfft, fs=fs, noverlap=0, scaling='spectrum')
-    t = (pd.date_range(ts.index[0], freq=freq, periods=spec.shape[1]) +
-         pd.to_timedelta(freq)/2.0).to_pydatetime()
-    im = ax.pcolormesh(t, 1/f[1:], spec[1:], cmap='Greys', norm=norm)
+    ts = util.tid.load_pituffik_tides().resample('1H').mean()
+    ts = ts.interpolate().diff()[1:]/3.6
+    _, _, _, im = ax.specgram(
+        ts, xextent=mdates.date2num([ts.index[0], ts.index[-1]]), **kwargs)
     ax.text(0.95, 0.2, 'Tide', color='k', transform=ax.transAxes,
             bbox=dict(ec='none', fc='w', alpha=0.75, pad=1))
 
-    # add colorbar
-    cb = fig.colorbar(im, cax=cax, extend='both')
-    cb.set_label(r'pressure change power spectrum ($Pa^2\,s^{-2}$)', labelpad=0)
+    # plot invisible timeseries to format axes as pandas
+    # (the 1D frequency ensures compatibility with mdates.date2num)
+    ts.resample('1D').mean().plot(ax=ax, visible=False)
 
     # set axes properties
-    grid[4].set_ylabel('period (h)')
+    ax.set_ylim(2.5/24, 0.5/24)
+    ax.set_yticks([1/24, 1/12])
+    ax.set_yticklabels(['24', '12'])
+
+    # add colorbar
+    cb = fig.colorbar(im, cax=cax, extend='both')
+    cb.set_label(r'pressure change ($Pa\,s^{-1}$) power spectral density (dB)')
+
+    # set axes properties
+    grid[5].set_ylabel('period (h)')
 
     # save
     fig.savefig(__file__[:-3])
