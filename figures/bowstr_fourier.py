93a08a4c1bcc2e747f64e56dae9c1dd378cee124
Julien Seguinot <juseg@posteo.eu>
2021-03-17 19:16:49 +0100

    Rework and rename fft fig (insets, etc).

---
+++ figures/bowstr_fourier.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# Copyright (c) 2019-2021, Julien Seguinot (juseg.github.io)
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
+
+"""Plot Bowdoin stress fourier transforms."""
+
+import numpy as np
+import pandas as pd
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+import absplots as apl
+import util.str
+
+
+def fourier(series):
+    """Compute fourier transform ready for plotting."""
+
+    # interpolate, drop nans, and differentiate
+    series = series.interpolate(limit_area='inside')
+    series = series.dropna()
+    series = series.diff()[1:]/3.6
+
+    # prepare frequencies and periods
+    frequency = np.fft.rfftfreq(series.shape[-1], 1)
+
+    # compute real fft amplitude
+    rfft = np.fft.rfft(series.values)
+    amplitude = np.abs(rfft)
+
+    # to compute spectral power or gain
+    # power = amplitude**2
+    # gain = 10*np.log10(power)
+
+    # remove zero frequency, convert to periods
+    period = (1/frequency[1:]) * (series.index.freq/pd.to_timedelta('1D'))
+    amplitude = amplitude[1:]
+
+    # return periods and amplitude
+    return period, amplitude
+
+
+def main():
+    """Main program called during execution."""
+
+    # initialize figure
+    fig = apl.figure_mm(figsize=(180, 120))
+    axes = np.array([fig.subplots_mm(  # 40x25 mm panels
+        nrows=3, ncols=4, sharex=True, sharey=True, gridspec_kw=dict(
+            left=10, right=2.5, bottom=12.5, top=2.5, hspace=2.5, wspace=2.5)),
+                     fig.subplots_mm(  # 20x10 mm panels
+        nrows=3, ncols=4, sharex=True, sharey=True, gridspec_kw=dict(
+            left=27.5, right=5, bottom=15, top=20, hspace=22.5, wspace=22.5))])
+
+    # load pressure and freezing dates
+    depth = util.str.load(variable='dept').iloc[0]
+    pres = util.str.load().resample('1H').mean()
+    date = util.str.load_freezing_dates()
+
+    # for each tilt unit
+    for i, unit in enumerate(pres):
+        color = 'C{}'.format(i)
+
+        # plot amplitude spectrum
+        for ax in axes[:, :, :3].reshape(2, -1)[:, i]:
+            ax.plot(*fourier(pres[unit][date[unit]:]), color=color)
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+
+        # add corner tag
+        ax = axes[0, :, :3].flat[i]
+        ax.text(0.95, 0.9, r'{}, {:.0f}$\,$m'.format(unit, depth[unit]),
+                color=color, fontsize=6, fontweight='bold',
+                transform=ax.transAxes, ha='right')
+
+    # plot tide data
+    tide = util.str.load_pituffik_tides().resample('1H').mean() / 10
+    for ax in axes[:, -1, -1]:
+        ax.plot(*fourier(tide), c='C9')
+
+    # add corner tag
+    ax = axes[0, -1, -1]
+    ax.text(0.95, 0.05, 'Pituffik\ntide'+r'$\,/\,$10', color='C9', fontsize=6,
+            fontweight='bold', ha='right', transform=ax.transAxes)
+
+    # move tide inset axes
+    ax = axes[1, -1, -1]
+    ax.set_position(ax.get_position().translated(0, 15/120))
+
+    # set (all) inset axes properties
+    ax.set_xlim(0.4, 1.2)
+    ax.set_ylim(10**-0.8, 10**3.4)
+    ax.set_xticks([0.5, 0.5*30/29, 1])
+    ax.set_xticklabels([])
+    ax.set_xticklabels([], minor=True)
+    ax.set_yticklabels([])
+
+    # set labels
+    axes[0, 2, 2].set_xlabel('period (h)', x=-1.25/40)
+    axes[0, 0, 2].set_ylabel(
+        r'amplitude of stress change after refreeze ($Pa\,s^{-1}$)',
+        y=-1.25/25)
+    axes[0, 0, 2].yaxis.set_label_position("right")
+
+    # mark one inset
+    mark_inset(*axes[:, 0, 0], loc1=1, loc2=3, ec='0.75', ls='--')
+
+    # remove unused axes
+    for ax in axes[:, :2, 3].flat:
+        ax.set_visible(False)
+
+    # save
+    fig.savefig(__file__[:-3])
+
+
+if __name__ == '__main__':
+    main()
