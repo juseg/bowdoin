5a0d89c3a9c6ef75405d74526d890929fad496bf
Julien Seguinot <juseg@posteo.eu>
2021-03-21 18:10:43 +0100

    Rework rolling correlations (specgram style etc).

--- figures/bowstr_rollcorr.py
+++ figures/bowstr_rollcorr.py
@@ -3,37 +3,25 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-import util.str
+"""Plot Bowdoin stress rolling-window cross-correlation."""
+
 import numpy as np
 import pandas as pd
-import absplots as apl
+import matplotlib as mpl
+import util.str
 
 
-def crosscorr(series, other, window=36):
-    """Return cross correlation between two series."""
-    shifts = np.arange(-window/2, window/2+1)
-    df = pd.DataFrame(
+def crosscorr(series, other, wmin=-30, wmax=6):
+    """Compute cross-correlation between two series."""
+    shifts = np.arange(wmin, wmax+1)
+    dataframe = pd.DataFrame(
         data=[series.shift(i, freq='infer') for i in shifts],
         index=pd.to_timedelta(shifts*series.index.freq))
-    return df.corrwith(other, axis=1)
-
-
-def crosscorr_df(dataframe):
-    """Return cross correlation between series of a dataframe."""
-    other = dataframe[dataframe.columns[0]]
-    df = pd.DataFrame(
-        data=[crosscorr(series, other) for series in dataframe])
-    return df
-
-
-def groupcorr(series, other):
-    grouped = series.groupby(series.index.month)
-    applied = grouped.apply(crosscorr, other)
-    applied = applied.unstack(level=0)
-    return applied
+    return dataframe.corrwith(other, axis=1)
 
 
 def rollcorr(series, other, window='14D', stride='7D'):
+    """Compute rolling-window cross-correlation between two series."""
     window = pd.to_timedelta(window)
     starts = pd.date_range(
         start=series.index[0], end=series.index[-1]-window, freq=stride)
@@ -49,40 +37,51 @@ def main():
     """Main program called during execution."""
 
     # initialize figure
-    pd.plotting.register_matplotlib_converters()
-    fig, axes = apl.subplots_mm(
-        figsize=(180, 120), nrows=4, sharex=True, gridspec_kw=dict(
-            left=12.5, right=12.5, bottom=12.5, top=2.5, hspace=2.5))
+    fig, axes = util.str.subplots_specgram(nrows=7)
 
     # load pressure data
-    pres = util.str.load()['20150101':'20170101']
-    pres = pres.resample('10T').mean().interpolate()
-    pres = pres[['UI07', 'UI06', 'UI05', 'UI04']]
+    depth = util.str.load(variable='dept').iloc[0]
+    pres = util.str.load().resample('10T').mean()  # kPa
+    pres = pres.interpolate(limit_area='inside')
     pres = util.str.filter(pres, cutoff=(1/6/12, 1/6), btype='bandpass')
+
+    # load tide data
+    tide = util.str.load_pituffik_tides().resample('10T').mean() / 10  # kPa/10
+
+    # subset
+    pres = pres.drop(columns=['UI03', 'UI02'])
+    # pres = pres['20140901':'20150901']  # first year looks good
+    # tide = tide['20140901':'20150901']  # things get messy then
 
     # for each unit
     for i, unit in enumerate(pres):
-        color = 'C{}'.format(i)
-        ts = pres[unit]
+        color = 'C{}'.format(i+2*(i > 3))
+        series = pres[unit].dropna()
 
         # plot cross correlation
         ax = axes[i]
-        corr = rollcorr(ts, pres['UI07'])
-        ax.pcolormesh(corr.columns, corr.index.total_seconds()/3600, corr,
-                      alpha=0.75, cmap='Greys', shading='auto', vmin=0, vmax=1)
+        corr = rollcorr(series, tide)
+        ax.imshow(
+            corr, alpha=0.75, aspect='auto', cmap='RdGy_r', vmin=-1, vmax=1,
+            extent=(*mpl.dates.date2num((corr.columns[0], corr.columns[-1])),
+                    *-corr.index[[-1, 0]].total_seconds()/3600))
 
-        # find maximum correlation
-        xlim = ax.get_xlim()
-        shift = corr.idxmax().dt.total_seconds()/3600
-        shift.plot(ax=ax, color=color, drawstyle='steps-mid')
-        ax.set_xlim(xlim)
+        # find maximum (anti)correlation
+        delay = -np.abs(corr).idxmax().dt.total_seconds()/3600
+        delay = delay.where(np.abs(corr).max() >= 0.5)
+        delay = delay.resample('1D').nearest()  # for compat with mpl.dates
+        delay.plot(ax=ax, drawstyle='steps-mid', color='w', lw=2, alpha=0.5)
+        delay.plot(ax=ax, drawstyle='steps-mid', color=color)
 
-        # deactivate grids
-        ax.grid(False)
+        # add text label
+        ax.text(
+            1.01, 0, unit+'\n'+r'{:.0f}$\,$m'.format(depth[unit]),
+            color=color, fontsize=6, fontweight='bold', transform=ax.transAxes)
 
     # set axes properties
-    ax.set_ylabel('phase shift (h)')
-    ax.set_xlabel('date')
+    ax.set_xlim('20140615', '20170815')
+    ax.set_yticks([0, 2, 4])
+    axes[len(axes)//2].set_ylabel('phase delay (h)')
 
     # save
     fig.savefig(__file__[:-3])
