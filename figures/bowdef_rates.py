d124097c0f724bd6f7423a7f3f9813819427a2bf
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-17 17:30:58 +0900

    Rework with stress utils and colours.

--- figures/bowdef_rates.py
+++ figures/bowdef_rates.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
-# Copyright (c) 2019-2021, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2019-2025, Julien Seguinot (juseg.dev)
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
 """Plot Bowdoin strain rate time series."""
 
 import absplots as apl
-import bowtem_utils
+import numpy as np
+import pandas as pd
+
+import bowstr_utils
 
 
 def main():
@@ -14,22 +17,20 @@ def main():
 
     # initialize figure
     fig, ax = apl.subplots_mm(figsize=(180, 90), gridspec_kw={
-        'left': 17.5, 'right': 2.5, 'bottom': 12.5, 'top': 2.5})
+        'left': 15, 'right': 2.5, 'bottom': 10, 'top': 2.5})
 
-    # plot each borehole
-    for bh in ('bh1', 'bh3'):
-
-        # load strain rate
-        exz = bowtem_utils.load_strain_rate(bh)*60*60*24*365.25
-
-        # plot strain rate
-        exz.plot(ax=ax)
+    # plot strain rate as sin(tilt) = sqrt(1-(cos(tx)*cos(ty))**2)
+    tilx = bowstr_utils.load(variable='tilx').resample('1D').mean().diff()
+    tily = bowstr_utils.load(variable='tily').resample('1D').mean().diff()
+    strain = (1 - (np.cos(tilx) * np.cos(tily)) ** 2) ** 0.5
+    strain = strain[strain.index >= '2014-07-17']
+    strain *= 3600 * 24 * 365.25 / pd.to_timedelta('1D').total_seconds()
+    strain.plot(ax=ax, xlabel='', ylabel=r'strain rate ($a^{-1}$)')
 
     # set axes properties
-    ax.legend(ncol=2)
-    ax.set_ylabel(r'strain rate ($a^{-1}$)')
-    ax.set_xlim('20140615', '20170815')
-    ax.set_ylim(0, 0.35)
+    ax.legend(loc='upper right', ncols=2)
+    ax.set_xlim('20140701', '20170801')
+    ax.set_ylim(-0.01, 0.33)
 
     # save
     fig.savefig(__file__[:-3])
