6b5fa7752c5d734f6ab779539dd579fff11a3668
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-17 16:14:01 +0900

    Add inset with x and y tilt components.

--- figures/bowdef_tilts.py
+++ figures/bowdef_tilts.py
@@ -17,18 +17,20 @@ def main():
 
     # initialize figure
     fig, ax = apl.subplots_mm(figsize=(180, 90), gridspec_kw={
-            'left': 17.5, 'right': 2.5, 'bottom': 12.5, 'top': 2.5})
+            'left': 15, 'right': 2.5, 'bottom': 10, 'top': 2.5})
+    inset = fig.add_axes_mm([17.5, 50, 35, 35])
 
     # plot tilt angular distance
     tilx = bowstr_utils.load(variable='tilx')
     tily = bowstr_utils.load(variable='tily')
+    tilx = tilx[tilx.index >= '2014-07-17']
+    tily = tily[tily.index >= '2014-07-17']
     tilt = np.arccos(np.cos(tilx)*np.cos(tily)) * 180 / np.pi
-    tilt = tilt[tilt.index >= '2014-07-17']
-    tilt.plot(ax=ax, xlabel='', ylabel='tilt angle (°)')
+    tilt.plot(ax=ax, xlabel='', ylabel='tilt angular distance (°)')
 
     # set axes properties
     ax.set_xlim('2014-07-01', '2017-08-01')
-    ax.legend(ncols=3)
+    ax.legend(loc='lower right', ncols=3)
 
     # format date axis (FIXME duplicates bowtem_timeseries)
     locator = mpl.dates.MonthLocator([1, 4, 7, 10])
@@ -39,6 +41,19 @@ def main():
     ax.xaxis.set_minor_locator(mpl.dates.MonthLocator())
     for label in ax.get_xticklabels(which='major'):
         label.set(rotation=0, horizontalalignment='center')
+
+    # plot directional components
+    inset.plot(tilx, tily)
+    tilx = tilx.resample('3ME').mean()
+    tily = tily.resample('3ME').mean()
+    inset.set_prop_cycle(None)
+    inset.plot(tilx, tily, ls='none', marker='o', mfc='none')
+
+    # set axes properties
+    inset.set_xlabel('x-tilt')
+    inset.set_ylabel('y-tilt')
+    inset.yaxis.set_label_position('right')
+    inset.yaxis.tick_right()
 
     # save
     fig.savefig(__file__[:-3])
