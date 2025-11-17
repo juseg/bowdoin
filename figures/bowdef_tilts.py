bf176672e986529b84c9cc03ccb1eb4a0342e099
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-17 16:45:21 +0900

    Revive the tilts. (#23)
    * Move bowdef_ts_tilt.py to bowdef_tilts.py.
    
    * Rework figure using Py3, stress loader.
    
    * Add inset with x and y tilt components.
    
    * Add subfigure labels.
    
    * Update figures Makefile.

---
+++ figures/bowdef_tilts.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# Copyright (c) 2015-2025, Julien Seguinot (juseg.dev)
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
+
+"""Plot Bowdoin stress filtered line plots."""
+
+import absplots as apl
+import matplotlib as mpl
+import numpy as np
+
+import bowstr_utils
+import bowtem_utils
+
+
+def main():
+    """Main program called during execution."""
+
+    # initialize figure
+    fig, ax = apl.subplots_mm(figsize=(180, 90), gridspec_kw={
+            'left': 15, 'right': 2.5, 'bottom': 10, 'top': 2.5})
+    inset = fig.add_axes_mm([27.5, 55, 40, 30])
+    bowtem_utils.add_subfig_label('(a)', ax=ax)
+    bowtem_utils.add_subfig_label('(b)', ax=inset)
+
+    # plot tilt angular distance
+    tilx = bowstr_utils.load(variable='tilx')
+    tily = bowstr_utils.load(variable='tily')
+    tilx = tilx[tilx.index >= '2014-07-17']
+    tily = tily[tily.index >= '2014-07-17']
+    tilt = np.arccos(np.cos(tilx)*np.cos(tily)) * 180 / np.pi
+    tilt.plot(ax=ax, xlabel='', ylabel='tilt angular distance (°)')
+
+    # set axes properties
+    ax.set_xlim('2014-07-01', '2017-08-01')
+    ax.legend(loc='lower right', ncols=3)
+
+    # format date axis (FIXME duplicates bowtem_timeseries)
+    locator = mpl.dates.MonthLocator([1, 4, 7, 10])
+    formatter = mpl.dates.ConciseDateFormatter(
+        locator, formats=['%b\n%Y', '%b']+['']*4, show_offset=False)
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+    ax.xaxis.set_minor_locator(mpl.dates.MonthLocator())
+    for label in ax.get_xticklabels(which='major'):
+        label.set(rotation=0, horizontalalignment='center')
+
+    # plot directional components
+    inset.plot(tilx, tily)
+    tilx = tilx.resample('3ME').mean()
+    tily = tily.resample('3ME').mean()
+    inset.set_prop_cycle(None)
+    inset.plot(tilx, tily, ls='none', marker='o', mfc='none')
+
+    # set axes properties
+    inset.set_aspect(1, adjustable='datalim')
+    inset.set_xlabel('x-tilt')
+    inset.set_ylabel('y-tilt')
+    inset.yaxis.set_label_position('right')
+    inset.yaxis.tick_right()
+
+    # save
+    fig.savefig(__file__[:-3])
+
+
+if __name__ == '__main__':
+    main()
