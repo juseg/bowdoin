6f2a1889c82644d8e5bb971de4df8567f1501a3a
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-17 15:48:42 +0900

    Rework figure using Py3, stress loader.

--- figures/bowdef_tilts.py
+++ figures/bowdef_tilts.py
@@ -1,28 +1,48 @@
-#!/usr/bin/env python2
-# coding: utf-8
+#!/usr/bin/env python
+# Copyright (c) 2015-2025, Julien Seguinot (juseg.dev)
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-import matplotlib.pyplot as plt
-import bowdef_utils
+"""Plot Bowdoin stress filtered line plots."""
 
-refdate = '2014-11-01'
+import absplots as apl
+import matplotlib as mpl
+import numpy as np
 
-# initialize figure
-fig, grid = plt.subplots(2, 1, sharex=True, sharey=True)
-grid[0].set_title('tilt angle ($^{\circ}$)')
+import bowstr_utils
 
-# for each borehole
-for i, bh in enumerate(bowdef_utils.boreholes):
-    ax = grid[i]
-    c = bowdef_utils.colors[bh]
 
-    # plot tilt angle
-    tilt = bowdef_utils.load_total_strain(bh, refdate, as_angle=True).resample('1D').mean()
-    tilt.plot(ax=ax, c=c, legend=False)
+def main():
+    """Main program called during execution."""
 
-    # set title
-    ax.axvspan('2014-06-01', refdate, ec='none', fc='0.9')
+    # initialize figure
+    fig, ax = apl.subplots_mm(figsize=(180, 90), gridspec_kw={
+            'left': 17.5, 'right': 2.5, 'bottom': 12.5, 'top': 2.5})
+
+    # plot tilt angular distance
+    tilx = bowstr_utils.load(variable='tilx')
+    tily = bowstr_utils.load(variable='tily')
+    tilt = np.arccos(np.cos(tilx)*np.cos(tily)) * 180 / np.pi
+    tilt = tilt[tilt.index >= '2014-07-17']
+    tilt.plot(ax=ax, xlabel='', ylabel='tilt angle (°)')
+
+    # set axes properties
     ax.set_xlim('2014-07-01', '2017-08-01')
-    ax.set_ylabel(bh)
+    ax.legend(ncols=3)
 
-# save
-bowdef_utils.savefig(fig)
+    # format date axis (FIXME duplicates bowtem_timeseries)
+    locator = mpl.dates.MonthLocator([1, 4, 7, 10])
+    formatter = mpl.dates.ConciseDateFormatter(
+        locator, formats=['%b\n%Y', '%b']+['']*4, show_offset=False)
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+    ax.xaxis.set_minor_locator(mpl.dates.MonthLocator())
+    for label in ax.get_xticklabels(which='major'):
+        label.set(rotation=0, horizontalalignment='center')
+
+    # save
+    fig.savefig(__file__[:-3])
+
+
+if __name__ == '__main__':
+    main()
