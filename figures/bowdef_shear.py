ec5fde218f26dc214998e9089b4fbf6675526190
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-19 11:54:25 +0900

    Update figure with new loader and colours.

--- figures/bowdef_shear.py
+++ figures/bowdef_shear.py
@@ -1,44 +1,61 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+# Copyright (c) 2015-2025, Julien Seguinot (juseg.dev)
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
+
+"""Plot Bowdoin deformation shear profiles."""
+
+
+import absplots as apl
+import numpy as np
 
 import bowdef_utils
+import bowstr_utils
 
 # dates to plot
 start = '2014-11-01'
 end = '2015-11-01'
 
-# initialize figure
-fig, grid = bowdef_utils.subplots_mm(nrows=1, ncols=2, sharex=True, sharey=True)
 
-# for each borehole
-for i, bh in enumerate(bowdef_utils.boreholes):
-    ax = grid[1-i]
-    c = bowdef_utils.colors[bh]
+def main():
+    """Main program called during execution."""
 
-    # read data values
-    depth = bowdef_utils.load_depth('tiltunit', bh).squeeze()
-    depth_base = bowdef_utils.load_depth('pressure', bh).squeeze()
-    exz = bowdef_utils.load_total_strain(bh, start, end)
+    # initialize figure
+    fig, axes = apl.subplots_mm(
+        figsize=(180, 90), ncols=2, sharex=True, sharey=True, gridspec_kw={
+            'left': 15, 'bottom': 10, 'right': 2.5, 'top': 2.5, 'wspace': 2.5})
 
-    # remove null values
-    notnull = exz.notnull()
-    depth = depth[notnull]
-    exz = exz[notnull]
+    # load total strain (do we need an util)
+    depth = bowstr_utils.load(variable='dept').iloc[0]
+    base = bowstr_utils.load(variable='base').iloc[0]
+    tilx = bowstr_utils.load(variable='tilx')
+    tily = bowstr_utils.load(variable='tily')
+    tilx = tilx.loc[end].mean() - tilx.loc[start].mean()
+    tily = tily.loc[end].mean() - tily.loc[start].mean()
+    strain = (1 - (np.cos(tilx) * np.cos(tily)) ** 2) ** 0.5
 
     # plot velocity profile
-    bowdef_utils.plot_vsia_profile(depth, exz, depth_base, ax=ax, c=c)
+    for ax, bh in zip(axes, ('BH3', 'BH1')):
+        mask = strain.notnull() & strain.index.str.startswith(
+            'U' if bh == 'BH1' else 'L')
+        color = f'C{mask.argmax()}'
+        bowdef_utils.plot_vsia_profile(
+            depth[mask], strain[mask], base[f'{bh}B'], ax=ax, c=color)
+        ax.text(4, 20, bh, color=color, fontweight='bold')
 
     # set axes properties
-    ax.set_title(bh)
+    ax.xaxis.set_inverted(True)
+    ax.yaxis.set_inverted(True)
 
-# set axes properties
-ax.set_ylim(300.0, 0.0)
-ax.set_xlim(35.0, 0.0)
+    # add common labels
+    figw, figh = fig.get_size_inches()*25.4
+    xlabel = 'total ice deformation from %s to %s (m)' % (start, end)
+    fig.text(0.5, 2.5/figh, xlabel, ha='center')
+    fig.text(2.5/figw, 0.5, 'depth (m)', va='center', rotation='vertical')
 
-# add common labels
-figw, figh = fig.get_size_inches()*25.4
-xlabel = 'total ice deformation from %s to %s (m)' % (start, end)
-fig.text(0.5, 2.5/figh, xlabel, ha='center')
-fig.text(2.5/figw, 0.5, 'depth (m)', va='center', rotation='vertical')
+    # save
+    fig.savefig(__file__[:-3])
 
-# save
-bowdef_utils.savefig(fig)
+
+if __name__ == '__main__':
+    main()
