Fix factor two in strain computation. (#26)
* Add fixme for factor two in strain computations!

* Replace strain rate figure with tilt rates.

* Correct shear heating formula in temperature profile.

* Save new and old dissipation values in erratum.

figures/bowdef_rates.py
@@ -3,7 +3,7 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-"""Plot Bowdoin strain rate time series."""
+"""Plot Bowdoin deformation tilt rates."""
 
 import absplots as apl
 import numpy as np
@@ -19,18 +19,18 @@ def main():
     fig, ax = apl.subplots_mm(figsize=(180, 90), gridspec_kw={
         'left': 15, 'right': 2.5, 'bottom': 10, 'top': 2.5})
 
-    # plot strain rate as sin(tilt) = sqrt(1-(cos(tx)*cos(ty))**2)
+    # plot tilt rate
     tilx = bowstr_utils.load(variable='tilx').resample('1D').mean().diff()
     tily = bowstr_utils.load(variable='tily').resample('1D').mean().diff()
-    strain = (1 - (np.cos(tilx) * np.cos(tily)) ** 2) ** 0.5
-    strain = strain[strain.index >= '2014-07-17']
-    strain *= 3600 * 24 * 365.25 / pd.to_timedelta('1D').total_seconds()
-    strain.plot(ax=ax, xlabel='', ylabel=r'strain rate ($a^{-1}$)')
+    tilt = np.arccos(np.cos(tilx)*np.cos(tily)) * 180 / np.pi
+    tilt = tilt[tilt.index >= '2014-07-17']
+    tilt *= 3600 * 24 * 365.25 / pd.to_timedelta('1D').total_seconds()
+    tilt.plot(ax=ax, xlabel='', ylabel=r'tilt rate ($°\,a^{-1}$)')
 
     # set axes properties
     ax.legend(loc='upper right', ncols=2)
     ax.set_xlim('20140701', '20170801')
-    ax.set_ylim(-0.01, 0.33)
+    ax.set_ylim(-1, 21)
 
     # save
     fig.savefig(__file__[:-3])