a90910f0a80dc2d1f390fa71f79963f5e9265d45
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-25 19:20:50 +0900

    Restore shear velocity profiles figure. (#25)
    * Move bowdef_profiles.py to bowdef_shear.py.
    
    * Fix broken refs in plot_vsia_profile util.
    
    * Update figure with new loader and colours.
    
    * Clean up and lint figure script.
    
    * Move plotting util locally in script for now.
    
    * Also move compute utils to fig script for now.
    
    * Fix bug in ice softness computation!
    
    * Fix missing factor half in strain computation!
    
    * Change to a more simple plot style (wip).
    
    * Replace tilt arrows with small rectangles (wip).
    
    * Fix unit marker colors.
    
    * Simplify analysis code, do not compute A.
    
    * Address pylint complaints and adjust text labels.

--- figures/bowdef_utils.py
+++ figures/bowdef_utils.py
@@ -38,33 +38,6 @@ def longest_continuous(ts):
     return ts
 
 
-def powerfit(x, y, deg, **kwargs):
-    """Fit to a power law."""
-    logx = np.log(x)
-    logy = np.log(y)
-    p = np.polyfit(logx, logy, deg, **kwargs)
-    return p
-
-
-def glenfit(depth, exz, g=9.80665, rhoi=910.0, slope=0.03):
-    """Fit to a power law with exp(C) = A * (rhoi*g*slope)**n."""
-    # FIXME: the slope (sin alpha) is an approximate value from MEASURES
-    # FIXME: this results in very variable and sometimtes even negative values
-    # for n. The negative values cause divide by zero encountered in power
-    # runtime warnings in vsia(). A better approach would be to fix n = 3 and
-    # fit for C only
-    n, C = powerfit(depth, exz, 1)
-    A = np.exp(C) / (rhoi*g*slope)**n
-    return n, A
-
-
-def vsia(depth, depth_base, n, A, g=9.80665, rhoi=910.0, slope=0.03):
-    """Return simple horizontal shear velocity profile."""
-    C = A * (rhoi*g*slope)**n
-    v = 2*C/(n+1) * (depth_base**(n+1) - depth**(n+1))
-    return v
-
-
 # Methods to load borehole data
 # -----------------------------
 
@@ -310,46 +283,6 @@ def rolling_plot(ax, ts, window, c='b'):
     std = pd.rolling_std(ts, window)
     avg.plot(ax=ax, color=c, ls='-')
     ax.fill_between(avg.index, avg-2*std, avg+2*std, color=c, alpha=0.25)
-
-
-def plot_vsia_profile(depth, exz, depth_base, ax=None, c='k', n=101,
-                      annotate=True):
-    """Fit and plot tilt velocity profile."""
-
-    # get current axes if None provided
-    ax = ax or plt.gca()
-
-    # prepare depth vector for fitting curve
-    depth_fit = np.linspace(0.0, depth_base, n)
-
-    # fit to glen's law
-    n, A = al.glenfit(depth, exz)
-
-    # compute velocity profiles
-    v_fit = al.vsia(depth_fit, depth_base, n, A)
-    v_obs = al.vsia(depth, depth_base, n, A)
-
-    # plot fitted velocity profiles
-    ax.plot(v_fit, depth_fit, c=c)
-    ax.fill_betweenx(depth_fit, 0.0, v_fit, color=c, alpha=0.25)
-
-    # add velocity arrows at observation points
-    for d, v in zip(depth, v_obs):
-        ax.arrow(0.0, d, v, 0.0, fc='none', ec=c,
-                 head_width=5.0, head_length=1.0, length_includes_head=True)
-
-    # add tilt arrows
-    ax.quiver(v_obs, depth, -exz*2, np.sqrt(1-(2*exz)**2),
-              angles='xy', scale=5.0)
-
-    # add horizontal lines
-    ax.axhline(0.0, c='k')
-    ax.axhline(depth_base, c='k')
-
-    # add fit values
-    if annotate:
-        ax.text(0.05, 0.05, r'A=%.2e$\,Pa^{-n}\,s^{-2}$, n=%.2f' % (A, n),
-                transform=ax.transAxes)
 
 
 # Map elements
