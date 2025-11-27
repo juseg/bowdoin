c37e501b917cfc86eff7f9b18b29b39896134a80
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-27 14:34:57 +0900

    Add option for lombscargle periodogram (wip).

--- figures/bowstr_pgram.py
+++ figures/bowstr_pgram.py
@@ -7,11 +7,13 @@
 
 import numpy as np
 import pandas as pd
+import scipy as sp
 import bowstr_utils
 
 
-def fourier(series):
-    """Compute Fourier transform ready for plotting."""
+def compute_fft(series):
+    """Compute fast Fourier transform ready for plotting."""
+    # FIXME review this code, is diff() truly needed?
 
     # interpolate, drop nans, and differentiate
     series = series.dropna()
@@ -37,6 +39,34 @@ def fourier(series):
     return period, amplitude
 
 
+def compute_lsp(series, periods):
+    """Compute Lomb-Scargle periodogram ready for plotting."""
+
+    # interpolate, drop nans, and differentiate
+    series = series.interpolate(limit_area='inside')
+    series = series.dropna()
+    series = series.diff() / series.index.to_series().diff().dt.total_seconds()
+    series = series[1:]  # first value is nan after diff
+
+    # prepare frequencies and periods
+    frequency = 2 * np.pi / periods
+
+    # compute periodogram
+    time = (series.index-series.index[0]).total_seconds()
+    power = sp.signal.lombscargle(time, series, frequency)
+    # amplitude = (4*power/len(periods))**0.5  # maybe
+
+    # return spectral power
+    return periods / 24 / 3600, power
+
+
+def compute_periodogram(series, periods, method='fft'):
+    """Compute custom periodogram ready for plotting."""
+    func = globals().get(f'compute_{method}')
+    args = (series,) + (periods,) * (method == 'lsp')
+    return func(*args)
+
+
 def load(variable='press', **kwargs):
     """Load modified variables."""
     if variable == 'press':
@@ -51,7 +81,7 @@ def load(variable='press', **kwargs):
     return data
 
 
-def plot(variable='wlev'):
+def plot(variable='press', method='fft'):
     """Plot and return full figure for given options."""
 
     # initialize figure
@@ -66,31 +96,29 @@ def plot(variable='wlev'):
     for i, unit in enumerate(df):
         color = f'C{i}'
 
-        # plot amplitude spectrum
-        per, amp = fourier(df.loc[date.get(unit, None):, unit])
-        for ax in axes[i]:
-            ax.plot(per, amp, color=color)
+        # plot periodograms (FIXME replace LSP power with amplitude)
+        per, amp = compute_periodogram(
+            df.loc[date.get(unit, None):, unit],
+            np.logspace(-1, 3, 201)*24*3600, method=method)
+        axes[i, 0].plot(per, amp, color=color)
+        per, amp = compute_periodogram(
+            df.loc[date.get(unit, None):, unit],
+            np.logspace(-0.35, 0.1, 201)*24*3600, method=method)
+        axes[i, 1].plot(per, amp, color=color)
 
-        # add main axes text label
+        # set axes properties
+        axes[i, 1].set_ylim(np.array([-0.05, 1.05])*amp[per < 2].max())
         axes[i, 0].text(
             0.95, 0.35, 'Pituffik\ntide'r'$\,/\,$10' if unit == 'tide' else
             f'{unit}\n{depth[unit]:.0f}'r'$\,$m',
             color=color, fontsize=6, fontweight='bold',
             transform=axes[i, 0].transAxes, ha='right')
 
-        # set inset axes limits
-        axes[i, 1].set_ylim(np.array([-0.05, 1.05])*amp[per < 2].max())
-
-    # add main axes corner tag
+    # add labels
+    axes[-1, 0].set_xlabel('period (days)')
     axes[-1, 0].text(
         0.95, 0.1, r'Pituffik tide$\,/\,$10', color='C9', fontsize=6,
         fontweight='bold', ha='right', transform=axes[-1, 0].transAxes)
-
-    # set inset axes limites
-    axes[-1, 1].set_ylim(np.array([-0.05, 1.05])*amp[per < 2].max())
-
-    # set labels
-    axes[-1, 0].set_xlabel('period (days)')
     axes[-1, 0].yaxis.set_label_text(
         f'amplitude of {variable.replace('press', 'stress')} change\n'
         f'after borehole closure ({'kPa' if variable == 'stres' else 'k°'}'
@@ -102,9 +130,10 @@ def plot(variable='wlev'):
 
 def main():
     """Main program called during execution."""
-    filters = ['12hbp', '12hhp', '24hbp', '24hhp', 'deriv']  # FIXME 'phase'
+    methods = ['fft', 'lsp']
     variables = ['press', 'tilts']
-    plotter = bowstr_utils.MultiPlotter(plot, variables=variables)
+    plotter = bowstr_utils.MultiPlotter(
+        plot, variables=variables, methods=methods)
     plotter()
 
 
