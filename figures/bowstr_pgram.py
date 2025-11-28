bf83a9962ed0d1c3d39a676438b24efeef0972cc
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-11-28 15:13:57 +0900

    Clean up analysis code.

--- figures/bowstr_pgram.py
+++ figures/bowstr_pgram.py
@@ -16,56 +16,27 @@ import bowtem_utils
 
 
 def compute_fft(series):
-    """Compute fast Fourier transform ready for plotting."""
-    # FIXME review this code, is diff() truly needed?
-
-    # interpolate, drop nans, and differentiate
-    series = series.dropna()
-    series = series.diff() / series.index.to_series().diff().dt.total_seconds()
-    series = series[1:]  # first value is nan after diff
-
-    # prepare frequencies and periods
-    frequency = np.fft.rfftfreq(series.shape[-1], 1)
-
-    # compute real fft amplitude
-    rfft = np.fft.rfft(series.values)
-    amplitude = np.abs(rfft)
-
-    # to compute spectral power or gain
-    # power = amplitude**2
-    # gain = 10*np.log10(power)
-
-    # remove zero frequency, convert to periods
-    period = (1/frequency[1:]) * (series.index.freq/pd.to_timedelta('1D'))
-    amplitude = amplitude[1:]
-
-    # return periods and amplitude
+    """Compute fast Fourier transform periodogram."""
+    amplitude = np.abs(np.fft.rfft(series.values))[1:]
+    frequency = np.fft.rfftfreq(series.shape[-1], 1)[1:]
+    period = series.index.freq / pd.to_timedelta('1D') / frequency
     return period, amplitude
 
 
 def compute_lsp(series, periods):
     """Compute Lomb-Scargle periodogram ready for plotting."""
-
-    # interpolate, drop nans, and differentiate
-    series = series.interpolate(limit_area='inside')
-    series = series.dropna()
-    series = series.diff() / series.index.to_series().diff().dt.total_seconds()
-    series = series[1:]  # first value is nan after diff
-
-    # prepare frequencies and periods
     frequency = 2 * np.pi / periods
-
-    # compute periodogram
-    time = (series.index-series.index[0]).total_seconds()
+    time = (series.index - series.index[0]).total_seconds()
     power = sp.signal.lombscargle(time, series, frequency)
     amplitude = 2 * (power / len(periods))**0.5
-
-    # return periods and amplitude
     return periods / 24 / 3600, amplitude
 
 
 def compute_periodogram(series, periods, method='fft'):
     """Compute custom periodogram ready for plotting."""
+    series = series.dropna()
+    series = series.diff() / series.index.to_series().diff().dt.total_seconds()
+    series = series[1:]
     func = globals().get(f'compute_{method}')
     args = (series,) + (periods,) * (method == 'lsp')
     return func(*args)
