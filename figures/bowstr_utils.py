11ee8f006b266e9f899acc4164ea477ba745607d
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-12-19 16:49:11 +0100

    Make interp the only option in load_spectral.

--- figures/bowstr_utils.py
+++ figures/bowstr_utils.py
@@ -188,7 +188,7 @@ def load_pituffik_tides(start='2014-07', end='2017-08', unit='kPa'):
     raise ValueError(f"Invalid unit {unit}.")
 
 
-def load_spectral(interp=True, variable='st', **kwargs):
+def load_spectral(variable='st', **kwargs):
     """Load modified variables for spectral analysis."""
     if variable == 'st':
         data = load(interp=True, tide=True, variable='wlev', **kwargs)
