003c24162e6a5c75e00f1b93e6c9babd4b0884c2
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2026-01-14 16:16:02 +0100

    Renames and clean up tides figure. (#30)
    * Move bowstr_fjordtide.py to bowstr_tides.py.
    
    * Resample time series for prettier date ticks.
    
    * Clean up tides figure script.
    
    * Move the legend away from subfigure label.
    
    * Update figures makefile.

--- figures/bowstr_tides.py
+++ figures/bowstr_tides.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2019-2021, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2019-2026, Julien Seguinot (juseg.dev)
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
@@ -23,31 +23,28 @@ def main():
     thul = bowstr_utils.load_pituffik_tides(start='2015-07', end='2015-08')
     bowd = bowstr_utils.load_bowdoin_tides()
 
+    # plot time series
+    ax = grid[0]
+    thul.resample('10min').mean().plot(ax=ax, label='Pituffik tide')
+    bowd.resample('10min').mean().plot(ax=ax, label='Bowdoin tide')
+    ax.set_xlabel('')
+    ax.set_ylabel('Tide (kPa)', labelpad=0.0)
+    ax.set_xlim(bowd.index[[0, -1]].date)
+    ax.set_xlim(bowd.index[0].floor('D'), bowd.index[-1].ceil('D'))
+    ax.legend(loc='lower left')
+
     # crop Thule data and downsample Bowdoin
     thul = thul[bowd.index[0]:bowd.index[-1]]
     bowd = bowd.reindex_like(thul, method='nearest')
-
-    # plot time series
-    ax = grid[0]
-    thul.plot(ax=ax, label='Pituffik tide')
-    bowd.plot(ax=ax, label='Bowdoin tide')
-    ax.set_xlabel('Date', labelpad=-8.0)
-    ax.set_ylabel('Tide (kPa)', labelpad=0.0)
-    ax.legend()
 
     # plot scatter plot
     ax = grid[1]
     ax.scatter(thul, bowd, marker='+', alpha=0.25)
     ax.set_xlabel('Pituffik tide (kPa)')
-    ax.set_ylabel('Bowdoin tide (kPa)', labelpad=0.0)
+    ax.set_ylabel('Bowdoin tide (kPa)', labelpad=-6)
 
     # save
     fig.savefig(__file__[:-3])
-
-    # save alternative frames  # FIXME formalise presentation mode
-    # fig.savefig(__file__[:-3]+'_z1')
-    # ax.set_visible(False)
-    # fig.savefig(__file__[:-3]+'_z0')
 
 
 if __name__ == '__main__':
