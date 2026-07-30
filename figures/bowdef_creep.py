d069e37c3da7ce40c69850dd8f2636e912000bee
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2026-07-30 13:07:02 +0200

    Add missing data from schohn et al 2025.

--- figures/bowdef_creep.py
+++ figures/bowdef_creep.py
@@ -31,6 +31,35 @@ def compute_shear_profile(base, depth, exponent, constant):
     return shear
 
 
+def load_schohn_etal_2025():
+    """Load Schohn et al data in a dataframe."""
+    columns = [
+        'control', 'strain', 'strain_rate', 'strain_rate_error',
+        'shear_stress', 'shear_stress_error', 'water_content',
+        'water_content_err']
+    data = {
+        '1A': ['Stress',      0.094, 27.2, '0.7', 0.24, '<0.01', 1.55,  0.4],
+        '1B': ['Stress',      0.112,  9.9, '0.3', 0.15, '<0.01', 1.53, 0.26],
+        '1C': ['Stress',      0.137, 14.6, '1.2', 0.21, '<0.01', 1.65, 0.33],
+        '1D': ['Stress',      0.145,  8.8, '0.4', 0.18, '<0.01', 1.48, 0.29],
+        '2A': ['Strain rate', 0.069, 11.7, '0.3', 0.19, '<0.01', 1.54, 0.13],
+        '2B': ['Strain rate', 0.075,  2.6,'<0.1', 0.05, '<0.01', 1.62, 0.08],
+        '2C': ['Strain rate', 0.103,  8.9, '1.9', 0.16, '<0.01', 1.28, 0.18],
+        '3A': ['Stress',      0.126, 21.1, '0.7', 0.24, '<0.01', 1.57, 0.13],
+        '3B': ['Strain rate', 0.174,  4.6, '0.9', 0.09, '<0.01', 1.49, 0.16],
+        '4A': ['Stress',      0.119, 13.7, '1.0', 0.22, '<0.01', 1.47, 0.19],
+        '4B': ['Strain rate', 0.141,  7.3, '0.4', 0.15, '<0.01', 1.35, 0.11],
+        '4C': ['Strain rate', 0.189,  5.8, '0.4', 0.11, '<0.01', 1.27, 0.36],
+        '4D': ['Strain rate', 0.214,  3.9, '0.3', 0.12, '<0.01', 1.16, 0.25],
+        '5A': ['Stress',      0.114, 13.6, '1.5', 0.19, '<0.01', 0.74, 0.08],
+        '5B': ['Strain rate', 0.145,  6.2, '1.3', 0.11, '<0.01', 0.77, 0.08],
+        '5C': ['Strain rate', 0.162,  4.7, '1.0', 0.08, '<0.01',  0.8,  0.1],
+        '5D': ['Strain rate',  0.19,  5.3, '0.5', 0.09, '<0.01', 0.76, 0.14],
+        '6A': ['Strain rate', 0.161,  7.7, '0.5', 0.13, '<0.01', 1.84, 0.17],
+        '6B': ['Strain rate', 0.177,  4.2, '1.0', 0.09, '<0.01', 1.64, 0.17]}
+    return pd.DataFrame(data=data, index=columns).transpose().infer_objects()
+
+
 def plot_strain_stress(ax, stress, strain_rate, label, color=None):
     """Plot strain rate vs stress data and fit a power law."""
 
