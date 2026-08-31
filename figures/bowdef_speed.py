4bf00c16bb550270b2b4cd904e8d93f082d5e852
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2026-08-31 15:38:14 +0200

    Add function for power fit with nans.

--- figures/bowdef_speed.py
+++ figures/bowdef_speed.py
@@ -6,12 +6,20 @@
 """Plot Bowdoin deformation against GNSS velocity."""
 
 import absplots as apl
+import numpy as np
 import pandas as pd
 
 import bowdef_utils
-import bowdef_shear  # FIXME move imports to utils
 import bowstr_utils
 import bowtem_utils
+
+
+def compute_power_fit_nan(depth, strain):
+    """Fit to a power law strain = constant * depth ** exponent."""
+    log_strain = np.log(strain.dropna())
+    log_depth = np.log(depth.reindex(log_strain.index))
+    exponent, constant = np.polyfit(log_depth, log_strain, 1)
+    return exponent, np.exp(constant)
 
 
 def main():
@@ -54,7 +62,7 @@ def main():
         # compute shear and basal speed
         coefs = strain.apply(
             lambda series: pd.Series(
-                data=bowdef_shear.compute_power_fit(depth[mask], series[mask]),
+                data=compute_power_fit_nan(depth, series),
                 index=['exponent', 'constant']), axis=1)
         power = coefs.exponent + 1
         shear = 2 * coefs.constant / power * base[f'{bh}B']**power
