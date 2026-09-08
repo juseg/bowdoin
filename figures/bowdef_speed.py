e74b49aea5aa2f57e8b5c1a21f17595499bb21f2
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2026-09-08 15:02:48 +0200

    Move all computations before plots, finally.

--- figures/bowdef_speed.py
+++ figures/bowdef_speed.py
@@ -97,14 +97,9 @@ def main():
     index = shear.index.intersection(speed.index)
     ratio = 100 - 100 * shear.divide(speed, axis=0).reindex(index)
 
-    # plot surface speed
-    speed.plot(ax=axes[0], color='tab:orange')
-
-    # plot satellite velocities
+    # load satellite velocities
     landsat = load_landsat_velocities().assign(source='landsat')
     sentinel = load_sentinel_velocities().assign(source='sentinel')
-    plot_satellite(axes[0], landsat, color=mpl.color_sequences['tab20'][3], label='Landsat')
-    plot_satellite(axes[0], sentinel, color=mpl.color_sequences['tab20'][11], label='Sentinel')
     sat = pd.concat([landsat, sentinel])
 
     # compute slip ratio from satellite and propagate uncertainties
@@ -113,11 +108,14 @@ def main():
     sat_ratio_error = 100 * sat_shear_speed.multiply(
         1/(sat.speed-sat.error/2)-1/(sat.speed+sat.error/2), axis=0)
 
-    # plot shear and slip ratio from geopositioning
+    # plot surface speed, shear and slip ratio from geopositioning
+    speed.plot(ax=axes[0], color='tab:orange')
     shear.plot(ax=axes[1], color={'BH1': 'tab:blue', 'BH3': 'tab:pink'})
     ratio.plot(ax=axes[2], color={'BH1': 'tab:blue', 'BH3': 'tab:pink'}, legend=False)
 
-    # plot slip ratio from satellite
+    # plot surface speed and slip ratio from satellite
+    plot_satellite(axes[0], landsat, color=mpl.color_sequences['tab20'][3], label='Landsat')
+    plot_satellite(axes[0], sentinel, color=mpl.color_sequences['tab20'][11], label='Sentinel')
     for bh in ['BH3', 'BH1']:
         plot_satellite(axes[2], sat.assign(
             speed=sat_ratio_speed[bh], error=sat_ratio_error[bh]),
